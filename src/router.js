b25239c43dade42c77bfaa042ed41e1d19948520
Chris Li <chris.li.2046@gmail.com>
2017-11-28 18:44:31 -0800

    Removed signin logic by deleting RestrictedRoute component

--- src/router.js
+++ src/router.js
@@ -1,48 +1,24 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React, { PropTypes } from 'react';
+import { Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
 import App from './containers/App/App';
-import asyncComponent from './helpers/AsyncFunc';
+// import asyncComponent from './helpers/AsyncFunc';
 
-const RestrictedRoute = ({ component: Component, ...rest, isLoggedIn }) =>
-  <Route
-    {...rest}
-    render={props =>
-      isLoggedIn
-        ? <Component {...props} />
-        : <Redirect
-            to={{
-              pathname: '/signin',
-              state: { from: props.location }
-            }}
-          />}
-  />;
-const PublicRoutes = ({ history, isLoggedIn }) => {
-  return (
-    <ConnectedRouter history={history}>
-      <div>
-        <Route
-          exact
-          path={'/'}
-          component={asyncComponent(() => import('./containers/Page/signin'))}
-        />
-        <Route
-          exact
-          path={'/signin'}
-          component={asyncComponent(() => import('./containers/Page/signin'))}
-        />
-        <RestrictedRoute
-          path="/dashboard"
-          component={App}
-          isLoggedIn={isLoggedIn}
-        />
-      </div>
-    </ConnectedRouter>
-  );
+const PublicRoutes = ({ history }) => (
+  <ConnectedRouter history={history}>
+    <div>
+      <Route
+        path="/dashboard"
+        component={App}
+      />
+    </div>
+  </ConnectedRouter>
+);
+
+PublicRoutes.propTypes = {
+  history: PropTypes.object.isRequired,
 };
 
-export default connect(state => ({
-  isLoggedIn: state.Auth.get('idToken') !== null
-}))(PublicRoutes);
+export default PublicRoutes;
