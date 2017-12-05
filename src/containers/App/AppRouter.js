aa420cbf76ca084e96dc1bd64ce7f7b7e1a87796
Chris Li <chris.li.2046@gmail.com>
2017-12-05 14:37:32 -0800

    Fixed AppRouter to make rootSaga work

--- src/containers/App/AppRouter.js
+++ src/containers/App/AppRouter.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import asyncComponent from '../../helpers/AsyncFunc';
 
-class AppRouter extends React.PureComponent {
+class AppRouter extends React.Component {
   render() {
     const { url } = this.props;
     return (
