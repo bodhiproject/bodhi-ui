6ff592bf9979f33573a05d0e12e1e306ef857730
Chris Li <chris.li.2046@gmail.com>
2017-12-25 23:10:43 -0800

    Remove blockchain reference from oracle.js

--- src/components/bodhi/createTopic.js
+++ src/components/bodhi/createTopic.js
@@ -91,7 +91,6 @@ class CreateTopic extends React.Component {
     console.log(`hexString is ${hexString.length}`);
 
     if (hexString && hexString.length <= MAX_LEN_EVENTNAME_HEX) {
-      // form.validateFields(['title']);
       callback();
     } else {
       callback('Event name is too long.');
