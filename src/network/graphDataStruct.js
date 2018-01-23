dc78ad787efbea2be34158a935fcd3a876bb8ba4
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-23 15:51:25 +0700

    Add method to check if valid enum

--- src/network/graphDataStruct.js
+++ src/network/graphDataStruct.js
@@ -1,3 +1,19 @@
+const ENUMS = {
+  status: [
+    'CREATED',
+    'VOTING',
+    'WAITRESULT',
+    'OPENRESULTSET',
+    'PENDING',
+    'WITHDRAW',
+  ],
+
+  token: [
+    'QTUM',
+    'BOT',
+  ],
+};
+
 const FIELD_MAPPINGS = {
   allTopics: `
     version
@@ -56,6 +72,10 @@ const FIELD_MAPPINGS = {
     chainBlockNum
   `,
 };
+
+export function isValidEnum(key, value) {
+  return _.has(ENUMS, key) && _.has(ENUMS[key], value);
+}
 
 export function getQueryFields(queryName) {
   return FIELD_MAPPINGS[queryName];
