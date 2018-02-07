fd76c7b2c93e211e25cc0eb6368e9434c9ab09ad
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-07 17:39:56 +0700

    Add table

--- src/containers/transactionHistory.jsx
+++ src/containers/transactionHistory.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Col, Row, Radio, Input } from 'antd';
+import { Col, Row, Radio, Input, Table } from 'antd';
 
 import LayoutContentWrapper from '../components/utility/layoutWrapper';
 
@@ -17,6 +17,7 @@ class TransactionHistory extends React.PureComponent {
 
     this.renderRadioGroup = this.renderRadioGroup.bind(this);
     this.renderSearch = this.renderSearch.bind(this);
+    this.renderTable = this.renderTable.bind(this);
   }
 
   componentWillMount() {
@@ -35,6 +36,9 @@ class TransactionHistory extends React.PureComponent {
           <Col span={6} offset={6}>
             {this.renderSearch()}
           </Col>
+        </Row>
+        <Row>
+          {this.renderTable()}
         </Row>
       </LayoutContentWrapper>
     );
@@ -68,9 +72,70 @@ class TransactionHistory extends React.PureComponent {
         onSearch={(value) => console.log(value)}
         style={{
           width: '100%',
-          height: '50px',
+          height: '40px',
         }}
         enterButton
+      />
+    );
+  }
+
+  renderTable() {
+    const columns = [{
+      title: 'Status',
+      dataIndex: 'status',
+      sorter: (a, b) => a.status < b.status ? -1 : 1,
+    }, {
+      title: 'Coin',
+      dataIndex: 'coin',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.coin < b.coin ? -1 : 1,
+    }, {
+      title: 'Amount',
+      dataIndex: 'amount',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.amount - b.amount,
+    }, {
+      title: 'User',
+      dataIndex: 'user',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.user < b.user ? -1 : 1,
+    }];
+
+    const data = [{
+      key: '1',
+      status: 'Pending',
+      coin: 'QTUM',
+      amount: 1111,
+      user: 'user1',
+    }, {
+      key: '2',
+      status: 'Completed',
+      coin: 'QTUM',
+      amount: 2222,
+      user: 'user2',
+    }, {
+      key: '3',
+      status: 'Pending',
+      coin: 'BOT',
+      amount: 3333,
+      user: 'user3',
+    }, {
+      key: '4',
+      status: 'Completed',
+      coin: 'BOT',
+      amount: 4444,
+      user: 'user4',
+    }];
+    const bordered = true;
+
+    return (
+      <Table
+        columns={columns}
+        dataSource={data}
+        bordered={bordered}
+        style={{
+          marginTop: '32px',
+        }}
       />
     );
   }
