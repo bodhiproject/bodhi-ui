f43e1555bed06e6252708cc545ac499c8d0ca154
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-07 17:06:01 +0700

    Add radio group and search bar

--- src/containers/App/AppRouter.js
+++ src/containers/App/AppRouter.js
@@ -36,6 +36,11 @@ class AppRouter extends React.Component {
           path={`${url}/create-topic`}
           component={asyncComponent(() => import('../createTopic'))}
         />
+        <Route
+          exact
+          path={`${url}/history`}
+          component={asyncComponent(() => import('../transactionHistory'))}
+        />
       </Switch>
     );
   }
