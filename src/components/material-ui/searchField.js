798fb295f806b427987ed001c257eaa9e806a8ed
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-12 18:22:41 +0700

    Finish search component

---
+++ src/components/material-ui/searchField.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import SearchIcon from 'material-ui-icons/Search';
+import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+
+const styles = (theme) => ({
+  root: {
+    background: '#ffffff',
+    borderRadius: 8,
+    paddingTop: 4,
+    paddingBottom: 4,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+});
+
+class SearchField extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { searchQuery } = this.state;
+
+    return (
+      <div>
+        <Input
+          className={classes.root}
+          id="searchField"
+          placeholder="Search"
+          startAdornment={<InputAdornment position="start"><SearchIcon /></InputAdornment>}
+          disableUnderline
+        />
+      </div>
+    );
+  }
+}
+
+SearchField.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+SearchField.defaultProps = {
+};
+
+export default withStyles(styles)(SearchField);
