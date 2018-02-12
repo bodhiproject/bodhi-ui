48fc815d3ca08638e8b25a17081e084a5b01a545
Deric Walintukan <deric.walintukan@gmail.com>
2018-02-12 20:35:04 +0700

    Convert searchfield back to antd

--- src/components/material-ui/searchField.js
+++ src/components/material-ui/searchField.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { withStyles } from 'material-ui/styles';
-import Paper from 'material-ui/Paper';
-import TextField from 'material-ui/TextField';
-import SearchIcon from 'material-ui-icons/Search';
-import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+import { Input, Icon } from 'antd';
 
-const styles = (theme) => ({
+const { Search } = Input;
+
+const styles = {
   root: {
     width: '100%',
-    background: '#ffffff',
-    borderRadius: 8,
-    paddingTop: 4,
-    paddingBottom: 4,
-    paddingLeft: 16,
-    paddingRight: 16,
+    height: 40,
     fontSize: 14,
   },
-});
+};
 
 class SearchField extends React.Component {
   constructor(props, context) {
@@ -29,28 +22,22 @@ class SearchField extends React.Component {
   }
 
   render() {
-    const { classes } = this.props;
-    const { searchQuery } = this.state;
-
     return (
-      <div>
-        <Input
-          className={classes.root}
-          id="searchField"
-          placeholder="Search"
-          startAdornment={<InputAdornment position="start"><SearchIcon /></InputAdornment>}
-          disableUnderline
-        />
-      </div>
+      <Search
+        id="searchField"
+        size="large"
+        placeholder="Search"
+        onSearch={(value) => console.log(value)}
+        style={styles.root}
+      />
     );
   }
 }
 
 SearchField.propTypes = {
-  classes: PropTypes.object.isRequired,
 };
 
 SearchField.defaultProps = {
 };
 
-export default withStyles(styles)(SearchField);
+export default SearchField;
