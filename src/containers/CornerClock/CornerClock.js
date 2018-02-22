dd0035275edcd15991fff0d36f1a9d08efc86df5
liviazhang <liviazhang@youyiii.com>
2018-02-22 13:04:50 +0800

    add listener to network connection

--- src/containers/CornerClock/CornerClock.js
+++ src/containers/CornerClock/CornerClock.js
@@ -1,12 +1,31 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Card } from 'antd';
+import { Card, Icon } from 'antd';
 import { getShortLocalDateTimeString } from '../../helpers/utility';
 
 class CornerClock extends React.PureComponent {
   constructor(props) {
     super(props);
+
+    this.handleNetworkChange = this.handleNetworkChange.bind(this);
+    this.renderNetworkConnection = this.renderNetworkConnection.bind(this);
     this.render = this.render.bind(this);
+  }
+
+  componentDidMount() {
+    // Subscribe to changes
+    window.addEventListener('offline', (e) => this.handleNetworkChange());
+    window.addEventListener('online', (e) => this.handleNetworkChange());
+  }
+
+  componentWillUnmount() {
+    // Clean up listener
+    window.removeEventListener('offline', (e) => this.handleNetworkChange());
+    window.removeEventListener('online', (e) => this.handleNetworkChange());
+  }
+
+  handleNetworkChange() {
+    this.renderNetworkConnection();
   }
 
   render() {
@@ -17,10 +36,29 @@ class CornerClock extends React.PureComponent {
 
     return (
       <Card className="corner-block-wrapper">
+        {this.renderNetworkConnection()}
         <h2>{blockNum}</h2>
         <p>Current Block</p>
         <p>{blockTime}</p>
       </Card>
+    );
+  }
+
+  renderNetworkConnection() {
+    if (navigator.onLine) {
+      return (
+        <p>
+          <span>Online</span>
+          <Icon className="corner-network-icon online" type="check-circle" />
+        </p>
+      );
+    }
+
+    return (
+      <p>
+        <span>Offline</span>
+        <Icon className="corner-network-icon offline" type="close-circle" />
+      </p>
     );
   }
 }
