5d2dce4b06d0751a8150ff8c73411f07a3defae2
liviazhang <liviazhang@youyiii.com>
2018-02-23 15:54:09 +0800

    add container on app level

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Layout, LocaleProvider } from 'antd';
+import { LocaleProvider } from 'antd';
 import { IntlProvider } from 'react-intl';
 import { Debounce } from 'react-throttle';
 import { WindowResizeListener } from 'react-window-resize-listener';
 import { ThemeProvider } from 'styled-components';
 import { ApolloProvider } from 'react-apollo';
+import Reboot from 'material-ui/Reboot';
 
 import graphClient from '../../network/graphClient';
 import authAction from '../../redux/auth/actions';
@@ -16,11 +17,8 @@ import AppRouter from './AppRouter';
 import { AppLocale } from '../../index';
 import themes from '../../config/themes';
 import { themeConfig /* siteConfig */ } from '../../config';
-import AppHolder from './commonStyle';
 import AppLoad from '../appLoad';
-import './global.css';
 
-const { Content } = Layout;
 const { logout } = authAction;
 const { toggleAll } = appActions;
 
@@ -31,44 +29,26 @@ export class App extends React.PureComponent {
     return (
       <LocaleProvider locale={currentAppLocale.antd}>
         <IntlProvider locale={currentAppLocale.locale} messages={currentAppLocale.messages}>
-          <ThemeProvider theme={themes[themeConfig.theme]}>
-            <ApolloProvider client={graphClient}>
-              <AppHolder>
-                <Layout style={{ height: '100vh' }}>
-                  <Debounce time="1000" handler="onResize">
-                    <WindowResizeListener
-                      onResize={(windowSize) =>
-                        this.props.toggleAll(
-                          windowSize.windowWidth,
-                          windowSize.windowHeight
-                        )}
-                    />
-                  </Debounce>
-                  <AppLoad />
-                  <Topbar url={url} />
-                  <Layout style={{ flexDirection: 'row', overflowX: 'hidden' }}>
-                    <Layout
-                      className="isoContentMainLayout"
-                      style={{
-                        height: '100vh',
-                      }}
-                    >
-                      <Content
-                        className="isomorphicContent"
-                        style={{
-                          flexShrink: '0',
-                          background: '#f9f9f9',
-                        }}
-                      >
-                        <AppRouter url={url} />
-                      </Content>
-                    </Layout>
-                  </Layout>
-                  <CornerClock />
-                </Layout>
-              </AppHolder>
-            </ApolloProvider>
-          </ThemeProvider>
+          <ApolloProvider client={graphClient}>
+            <div className="root">
+              <Reboot />
+              <Debounce time="1000" handler="onResize">
+                <WindowResizeListener
+                  onResize={(windowSize) =>
+                    this.props.toggleAll(
+                      windowSize.windowWidth,
+                      windowSize.windowHeight
+                    )}
+                />
+              </Debounce>
+              <AppLoad />
+              <Topbar url={url} />
+              <div className="container">
+                <AppRouter url={url} />
+              </div>
+              <CornerClock />
+            </div>
+          </ApolloProvider>
         </IntlProvider>
       </LocaleProvider>
     );
