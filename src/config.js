648b73bb9fbf339a455c3ee196f911c9e6788226
liviazhang <liviazhang@youyiii.com>
2018-02-23 17:27:02 +0800

    remove unused theme

--- src/config.js
+++ src/config.js
@@ -8,17 +8,9 @@ const siteConfig = {
   footerText: 'Bodhi Prediction Market ©2017 Created by Bodhi.Network',
 };
 
-const themeConfig = {
-  topbar: 'themedefault',
-  sidebar: 'themedefault',
-  layout: 'themedefault',
-  theme: 'themeBodhi',
-};
-
 const language = 'english';
 
 export {
   siteConfig,
   language,
-  themeConfig,
 };
