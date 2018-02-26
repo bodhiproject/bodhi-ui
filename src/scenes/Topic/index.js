d349d6cf7c416c2f67803d9134e21eac05d715e0
Bowen Zhang <bowen@bohdi.network>
2018-02-26 13:52:36 -0800

    finish all translation stuffs

--- src/scenes/Topic/index.js
+++ src/scenes/Topic/index.js
@@ -2,7 +2,6 @@ import React, { PropTypes } from 'react';
 import { Row, Col, Breadcrumb } from 'antd';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { FormattedMessage } from 'react-intl';
 import _ from 'lodash';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 
