2bc7763e6b31230a5e97c3a777612e83406f74ac
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-01 21:11:45 +0700

    Change app actions name

--- src/redux/app/actions.js
+++ src/redux/app/actions.js
@@ -8,13 +8,13 @@ export function getView(width) {
   return newView;
 }
 
-const actions = {
+const appActions = {
   TOGGLE_ALL: 'TOGGLE_ALL',
   toggleAll: (width, height) => {
     const view = getView(width);
     const collapsed = view !== 'DesktopView';
     return {
-      type: actions.TOGGLE_ALL,
+      type: appActions.TOGGLE_ALL,
       collapsed,
       view,
       height,
@@ -24,24 +24,24 @@ const actions = {
   ADD_WALLET_ADDRESS: 'ADD_WALLET_ADDRESS',
   SELECT_WALLET_ADDRESS: 'SELECT_WALLET_ADDRESS',
   addWalletAddress: (value) => ({
-    type: actions.ADD_WALLET_ADDRESS,
+    type: appActions.ADD_WALLET_ADDRESS,
     value,
   }),
   selectWalletAddress: (value) => ({
-    type: actions.SELECT_WALLET_ADDRESS,
+    type: appActions.SELECT_WALLET_ADDRESS,
     value,
   }),
 
   LIST_UNSPENT: 'LIST_UNSPENT',
   LIST_UNSPENT_RETURN: 'LIST_UNSPENT_RETURN',
   listUnspent: () => ({
-    type: actions.LIST_UNSPENT,
+    type: appActions.LIST_UNSPENT,
   }),
 
   GET_BOT_BALANCE: 'GET_BOT_BALANCE',
   GET_BOT_BALANCE_RETURN: 'GET_BOT_BALANCE_RETURN',
   getBotBalance: (owner, senderAddress) => ({
-    type: actions.GET_BOT_BALANCE,
+    type: appActions.GET_BOT_BALANCE,
     payload: {
       owner,
       senderAddress,
@@ -50,31 +50,31 @@ const actions = {
 
   GET_SYNC_INFO: 'GET_SYNC_INFO',
   getSyncInfo: () => ({
-    type: actions.GET_SYNC_INFO,
+    type: appActions.GET_SYNC_INFO,
   }),
   ON_SYNC_INFO: 'ON_SYNC_INFO',
   onSyncInfo: (syncInfo) => ({
-    type: actions.ON_SYNC_INFO,
+    type: appActions.ON_SYNC_INFO,
     syncInfo,
   }),
   SYNC_INFO_RETURN: 'SYNC_INFO_RETURN',
 
   UPDATE_SYNC_PROGRESS: 'UPDATE_SYNC_PROGRESS',
   updateSyncProgress: (percentage) => ({
-    type: actions.UPDATE_SYNC_PROGRESS,
+    type: appActions.UPDATE_SYNC_PROGRESS,
     percentage,
   }),
 
   TOGGLE_INITIAL_SYNC: 'TOGGLE_INITIAL_SYNC',
   toggleInitialSync: (isSyncing) => ({
-    type: actions.TOGGLE_INITIAL_SYNC,
+    type: appActions.TOGGLE_INITIAL_SYNC,
     isSyncing,
   }),
 
   GET_INSIGHT_TOTALS: 'GET_INSIGHT_TOTALS',
   GET_INSIGHT_TOTALS_RETURN: 'GET_INSIGHT_TOTALS_RETURN',
   getInsightTotals: () => ({
-    type: actions.GET_INSIGHT_TOTALS,
+    type: appActions.GET_INSIGHT_TOTALS,
   }),
 };
-export default actions;
+export default appActions;
