b7fc33d24f5475537724159a7da6983c91df2041
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-03 01:50:30 +0700

    Add table and some styling

--- src/scenes/Wallet/index.jsx
+++ src/scenes/Wallet/index.jsx
@@ -4,13 +4,16 @@ import { connect } from 'react-redux';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Button from 'material-ui/Button';
+import ContentCopy from 'material-ui-icons/ContentCopy';
 import { withStyles } from 'material-ui/styles';
 import classNames from 'classnames';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 
 import styles from './styles';
 
-const mockData = [
+const mockTotals = [
   {
     token: 'QTUM',
     amount: 12345,
@@ -18,6 +21,28 @@ const mockData = [
   {
     token: 'BOT',
     amount: 67890,
+  },
+];
+const mockRows = [
+  {
+    address: 'qKjn4fStBaAtwGiwueJf9qFxgpbAvf1xAy',
+    qtum: 12345,
+    bot: 12345,
+  },
+  {
+    address: 'qKoxAUEQ1Nj6anwes6ZjRGQ7aqdiyUeat8',
+    qtum: 12345,
+    bot: 12345,
+  },
+  {
+    address: 'qTumW1fRyySwmoPi12LpFyeRj8W6mzUQA3',
+    qtum: 12345,
+    bot: 12345,
+  },
+  {
+    address: 'qbyAYsQQf7U4seauDv9cYjwfiNrR9fJz3R',
+    qtum: 12345,
+    bot: 12345,
   },
 ];
 
@@ -33,21 +58,71 @@ class MyWallet extends React.Component {
     const { classes } = this.props;
 
     return (
-      <Paper className={classes.myWalletPaper}>
-        <Grid container spacing={0}>
-          <Grid item className={classes.myWalletContainerGrid}>
-            <Typography variant="title" className={classes.myWalletTitle}>
-              <FormattedMessage id="myWallet.myBalance" />
-            </Typography>
-            <Grid container classname={classes.totalsContainerGrid}>
-              {mockData.map((item) => (
-                <Grid item key={item.token} className={classes.totalsItemGrid}>
-                  <Typography variant="display1" className={classes.totalsItemAmount}>{item.amount}</Typography>
-                  <Typography variant="body2">{item.token}</Typography>
-                </Grid>
-              ))}
-            </Grid>
+      <Paper className={classes.rootPaper}>
+        <Grid container spacing={0} className={classes.containerGrid}>
+          <Typography variant="title" className={classes.myBalanceTitle}>
+            <FormattedMessage id="myWallet.myBalance" />
+          </Typography>
+          <Grid container classname={classes.totalsContainerGrid}>
+            {mockTotals.map((item) => (
+              <Grid item key={item.token} className={classes.totalsItemGrid}>
+                <Typography variant="display1" className={classes.totalsItemAmount}>{item.amount}</Typography>
+                <Typography variant="body2">{item.token}</Typography>
+              </Grid>
+            ))}
           </Grid>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow className={classes.tableHeader}>
+                <TableCell>
+                  <Typography variant="body1" className={classes.tableHeaderItemText}>
+                    <FormattedMessage id="myWallet.address" />
+                  </Typography>
+                </TableCell>
+                <TableCell></TableCell>
+                <TableCell numeric>
+                  <Typography variant="body1" className={classes.tableHeaderItemText}>
+                    <FormattedMessage id="myWallet.qtum" />
+                  </Typography>
+                </TableCell>
+                <TableCell numeric>
+                  <Typography variant="body1" className={classes.tableHeaderItemText}>
+                    <FormattedMessage id="myWallet.bot" />
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1" className={classes.tableHeaderItemText}>
+                    <FormattedMessage id="myWallet.actions" />
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {mockRows.map((item) => (
+                <TableRow key={item.address}>
+                  <TableCell className={classes.tableRowCell}>
+                    <Typography variant="body1" className={classes.tableRowAddressText}>{item.address}</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Button size="small" className={classes.tableRowCopyButton}>
+                      <ContentCopy className={classes.tableRowCopyButtonIcon} />
+                      <FormattedMessage id="myWallet.copy" />
+                    </Button>
+                  </TableCell>
+                  <TableCell numeric>{item.qtum}</TableCell>
+                  <TableCell numeric>{item.bot}</TableCell>
+                  <TableCell>
+                    <Button variant="raised" size="small" className={classes.tableRowActionButton}>
+                      <FormattedMessage id="myWallet.deposit" />
+                    </Button>
+                    <Button variant="raised" size="small" className={classes.tableRowActionButton}>
+                      <FormattedMessage id="myWallet.withdraw" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </Grid>
       </Paper>
     );
--- src/scenes/Wallet/styles.js
+++ src/scenes/Wallet/styles.js
@@ -1,9 +1,9 @@
 const styles = (theme) => ({
-  myWalletPaper: {
+  rootPaper: {
     boxShadow: 'none',
     borderRadius: theme.borderRadius,
   },
-  myWalletContainerGrid: {
+  containerGrid: {
     padding: theme.padding.lg.px,
     overflowX: 'hidden',
     '&.right': {
@@ -11,14 +11,40 @@ const styles = (theme) => ({
       textAlign: 'right',
     },
   },
-  myWalletTitle: {
+  myBalanceTitle: {
     marginBottom: theme.padding.md.px,
+  },
+  totalsContainerGrid: {
+    marginBottom: theme.padding.lg.px,
   },
   totalsItemGrid: {
     marginRight: theme.padding.md.px,
   },
   totalsItemAmount: {
     marginBottom: theme.padding.unit.px,
+  },
+  table: {
+    marginTop: theme.padding.md.px,
+  },
+  tableHeader: {
+    background: theme.palette.background.grey,
+  },
+  tableHeaderItemText: {
+    fontWeight: theme.typography.fontWeightBold,
+  },
+  tableRowCell: {
+    paddingTop: theme.padding.sm.px,
+    paddingBottom: theme.padding.sm.px,
+  },
+  tableRowCopyButton: {
+    color: theme.palette.primary.main,
+  },
+  tableRowCopyButtonIcon: {
+    marginRight: theme.padding.unit.px,
+  },
+  tableRowActionButton: {
+    marginRight: theme.padding.xs.px,
+    borderRadius: 2,
   },
 });
 
