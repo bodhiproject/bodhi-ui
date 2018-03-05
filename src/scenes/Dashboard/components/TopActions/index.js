7245b7841cf2abaa3fd572c56cd48aa310daf17c
Livia Zhang <liviavinci@gmail.com>
2018-03-05 17:15:05 +0800

    change dashboard actions to new design

--- src/scenes/Dashboard/components/TopActions/index.js
+++ src/scenes/Dashboard/components/TopActions/index.js
@@ -1,80 +1,74 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Row, Col, Select, Button, Menu, Icon, Dropdown } from 'antd';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import Button from 'material-ui/Button';
+import Grid from 'material-ui/Grid';
+import AddIcon from 'material-ui-icons/Add';
+import { FormControl } from 'material-ui/Form';
+import Select from 'material-ui/Select';
+import { MenuItem } from 'material-ui/Menu';
+import Card from 'material-ui/Card';
+import { withStyles } from 'material-ui/styles';
 
 import dashboardActions from '../../../../redux/Dashboard/actions';
 import { SortBy } from '../../../../constants';
-
-const MenuItem = Menu.Item;
+import styles from './styles';
 
 class TopActions extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      sortOption: undefined,
+      sortOption: SortBy.Ascending,
     };
 
     this.onSortOptionSelected = this.onSortOptionSelected.bind(this);
   }
 
-  onSortOptionSelected(item) {
+  onSortOptionSelected(event) {
     this.setState({
-      sortOption: item.key,
+      sortOption: event.target.value,
     });
-    this.props.sortOrderChanged(item.key);
+
+    this.props.sortOrderChanged(event.target.value);
   }
 
   render() {
-    const sortMenu = (
-      <Menu onClick={this.onSortOptionSelected}>
-        <MenuItem key={SortBy.Ascending}><FormattedMessage id="sort.asc" defaultMessage="Ascending" /></MenuItem>
-        <MenuItem key={SortBy.Descending}><FormattedMessage id="sort.desc" defaultMessage="Descending" /></MenuItem>
-      </Menu>
-    );
-
-    let sortButtonText;
-    let sortButtonIcon;
-    if (this.state.sortOption === SortBy.Ascending) {
-      sortButtonText = <FormattedMessage id="sort.asc" defaultMessage="Ascending" />;
-      sortButtonIcon = <Icon type="arrow-up" />;
-    } else if (this.state.sortOption === SortBy.Descending) {
-      sortButtonText = <FormattedMessage id="sort.desc" defaultMessage="Descending" />;
-      sortButtonIcon = <Icon type="arrow-down" />;
-    } else {
-      sortButtonText = <FormattedMessage id="sort.default" defaultMessage="Sort" />;
-      sortButtonIcon = <Icon type="down-circle-o" />;
-    }
+    const { classes } = this.props;
 
     return (
-      <div className="tabBtnGroup">
-        <Row>
-          <Col xs={20}>
-            <Link to="/create-topic" >
-              <Button type="primary" size="large">
-                + <FormattedMessage id="create.title" defaultMessage="Create an event" />
-              </Button>
-            </Link>
-          </Col>
-          <Col xs={4}>
-            <div className="controlBtnGroup">
-              <Dropdown overlay={sortMenu}>
-                <Button size="large" style={{ width: 144 }}>
-                  {sortButtonText} {sortButtonIcon}
-                </Button>
-              </Dropdown>
-            </div>
-          </Col>
-        </Row>
-      </div>);
+      <Grid container className={classes.dashboardActionsWrapper}>
+        <Grid item xs={8}>
+          <Link to="/create-topic" >
+            <Button variant="raised" size="medium" color="primary" className={classes.createEventButton}>
+              <AddIcon fontSize />
+              <FormattedMessage id="create.title" defaultMessage="Create an event" />
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={4} className={classes.dashboardActionsRight}>
+          <span className={classes.dashboardActionsSortLabel}>
+            <FormattedMessage id="sort.label" defaultMessage="Sort By" />
+          </span>
+          <Card className={classes.dashboardActionsSort}>
+            <FormControl>
+              <Select disableUnderline value={this.state.sortOption} onChange={this.onSortOptionSelected}>
+                <MenuItem value={SortBy.Ascending}><FormattedMessage id="sort.asc" defaultMessage="Ascending" /></MenuItem>
+                <MenuItem value={SortBy.Descending}><FormattedMessage id="sort.desc" defaultMessage="Descending" /></MenuItem>
+              </Select>
+            </FormControl>
+          </Card>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
 TopActions.propTypes = {
+  classes: PropTypes.object.isRequired,
   sortOrderChanged: PropTypes.func,
 };
 
@@ -90,4 +84,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(TopActions));
+export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(withStyles(styles, { withTheme: true })(TopActions)));
