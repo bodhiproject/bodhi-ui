33ba127526c26488c315da6f66c86eb68b337586
Livia Zhang <liviavinci@gmail.com>
2018-03-07 19:32:55 +0800

    address review

--- src/scenes/Oracle/components/PredictionTxHistory/index.js
+++ src/scenes/Oracle/components/PredictionTxHistory/index.js
@@ -13,37 +13,38 @@ class PredictionTxHistory extends React.PureComponent {
   render() {
     const { classes, transactions, options } = this.props;
 
-    if (!transactions.length || !options.length) {
-      return null;
-    }
-
     return (
       <div className={classes.detailTxWrapper}>
         <Typography variant="headline" className={classes.detailTxTitle}>
           TRANSACTIONS
-        </Typography>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell padding="dense">Date</TableCell>
-              <TableCell padding="dense">Type</TableCell>
-              <TableCell padding="dense">Description</TableCell>
-              <TableCell padding="dense">Amount</TableCell>
-              <TableCell padding="dense">Status</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {_.map(transactions, (transaction, index) => (
-              <TableRow key={transaction.txid} selected={index % 2 === 0}>
-                <TableCell padding="dense">{getLocalDateTimeString(transaction.createdTime)}</TableCell>
-                <TableCell padding="dense">{transaction.type}</TableCell>
-                <TableCell padding="dense">{`#${transaction.optionIdx} ${options[transaction.optionIdx - 1]}`}</TableCell>
-                <TableCell padding="dense">{`${transaction.amount} ${transaction.token}`}</TableCell>
-                <TableCell padding="dense">{transaction.status}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        </Typography> {
+          transactions.length && options.length ?
+            (<Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell padding="dense">Date</TableCell>
+                  <TableCell padding="dense">Type</TableCell>
+                  <TableCell padding="dense">Description</TableCell>
+                  <TableCell padding="dense">Amount</TableCell>
+                  <TableCell padding="dense">Status</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {_.map(transactions, (transaction, index) => (
+                  <TableRow key={transaction.txid} selected={index % 2 === 0}>
+                    <TableCell padding="dense">{getLocalDateTimeString(transaction.createdTime)}</TableCell>
+                    <TableCell padding="dense">{transaction.type}</TableCell>
+                    <TableCell padding="dense">{`#${transaction.optionIdx} ${options[transaction.optionIdx - 1]}`}</TableCell>
+                    <TableCell padding="dense">{`${transaction.amount} ${transaction.token}`}</TableCell>
+                    <TableCell padding="dense">{transaction.status}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>) :
+            <Typography variant="body1">
+              You do not have any transactions right now.
+            </Typography>
+        }
       </div>
     );
   }
