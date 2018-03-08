45f92d47948ce075fc8ea14ff0e74e1d670e2f21
liviavinci <liviavinci@gmail.com>
2018-03-09 01:55:31 +0800

    more deletion

--- src/components/Progress/index.js
+++
@@ -1,8 +0,0 @@
-import { Progress } from 'antd';
-import AntProgress from './styles';
-import WithDirection from '../../config/withDirection';
-
-const WDProgress = AntProgress(Progress);
-const isoProgress = WithDirection(WDProgress);
-
-export default isoProgress;
