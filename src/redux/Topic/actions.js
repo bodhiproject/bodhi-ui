6a96ffa4f1b61c5260edaabd871c997c312ad1e2
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-13 18:36:04 +0700

    Add to actions

--- src/redux/Topic/actions.js
+++ src/redux/Topic/actions.js
@@ -1,4 +1,14 @@
 const topicActions = {
+  GET_BET_AND_VOTE_BALANCES: 'GET_BET_VOTE_BALANCES',
+  GET_BET_VOTE_BALANCES_RETURN: 'GET_BET_VOTE_BALANCES_RETURN',
+  getBetAndVoteBalances: (contractAddress, senderAddress) => ({
+    type: topicActions.GET_BET_VOTE_BALANCES,
+    params: {
+      contractAddress,
+      senderAddress,
+    }
+  }),
+
   CALCULATE_WINNINGS: 'CALCULATE_WINNINGS',
   CALCULATE_WINNINGS_RETURN: 'CALCULATE_WINNINGS_RETURN',
   calculateWinnings: (contractAddress, senderAddress) => ({
