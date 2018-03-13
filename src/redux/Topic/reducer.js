6ad777f2e4cd83cfefec3c31ef11577196da9b16
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-13 18:44:02 +0700

    Add reducer logic for balances

--- src/redux/Topic/reducer.js
+++ src/redux/Topic/reducer.js
@@ -2,13 +2,22 @@ import { Map } from 'immutable';
 import actions from './actions';
 
 const initState = new Map({
-  value: undefined,
+  betBalances: [],
+  voteBalances: [],
+  botWinnings: 0,
+  qtumWinnings: 0,
 });
 
 export default function topicReducer(state = initState, action) {
   switch (action.type) {
+    case actions.GET_BET_VOTE_BALANCES_RETURN: {
+      return state
+        .set('betBalances', action.value.bets)
+        .set('voteBalances', action.value.votes);
+    }
     case actions.CALCULATE_WINNINGS_RETURN: {
-      return state.set('botWinnings', action.value.botWon)
+      return state
+        .set('botWinnings', action.value.botWon)
         .set('qtumWinnings', action.value.qtumWon);
     }
     default: {
