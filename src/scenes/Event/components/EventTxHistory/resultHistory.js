d8ca72462ab537b42f69bf94a02fb274fb592588
liviavinci <liviavinci@gmail.com>
2018-03-14 17:56:57 +0800

    address review

--- src/scenes/Event/components/EventTxHistory/resultHistory.js
+++ src/scenes/Event/components/EventTxHistory/resultHistory.js
@@ -6,10 +6,17 @@ import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Ta
 import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 
+import { Token } from '../../../../constants';
 import { getLocalDateTimeString } from '../../../../helpers/utility';
+import { i18nToUpperCase } from '../../../../helpers/i18nUtil';
 import styles from './styles';
 
 class EventResultHistory extends React.PureComponent {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    oracles: PropTypes.array.isRequired,
+  };
+
   render() {
     const { classes, oracles } = this.props;
 
@@ -18,7 +25,9 @@ class EventResultHistory extends React.PureComponent {
     return (
       <div className={classes.detailTxWrapper}>
         <Typography variant="headline" className={classes.detailTxTitle}>
-          <FormattedMessage id="str.resultHistory" defaultMessage="RESULT HISTORY" />
+          <FormattedMessage id="str.resultHistory" defaultMessage="Result History">
+            {(txt) => i18nToUpperCase(txt)}
+          </FormattedMessage>
         </Typography> {
           sortedOracles.length ?
             (<Table>
@@ -42,7 +51,7 @@ class EventResultHistory extends React.PureComponent {
                 {_.map(sortedOracles, (oracle, index) => (
                   <TableRow key={`result-${index}`} selected={index % 2 === 1}>
                     <TableCell padding="dense">{getLocalDateTimeString(oracle.endTime)}</TableCell>
-                    <TableCell padding="dense">{oracle.token === 'QTUM' ? 'QTUM Predict' : `Voting Round ${index}`}</TableCell>
+                    <TableCell padding="dense">{oracle.token === Token.QTUM ? 'Betting Round' : `Voting Round ${index}`}</TableCell>
                     <TableCell padding="dense">{`#${oracle.resultIdx + 1} ${oracle.options[oracle.resultIdx]}`}</TableCell>
                     <TableCell padding="dense">{`${_.sum(oracle.amounts)} ${oracle.token}`}</TableCell>
                   </TableRow>
@@ -57,10 +66,5 @@ class EventResultHistory extends React.PureComponent {
     );
   }
 }
-
-EventResultHistory.propTypes = {
-  classes: PropTypes.object.isRequired,
-  oracles: PropTypes.array.isRequired,
-};
 
 export default withStyles(styles, { withTheme: true })(injectIntl(EventResultHistory));
