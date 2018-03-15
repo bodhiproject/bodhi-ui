2d7cf08197d4f09b74a4ef44608322182e65b017
Deric Walintukan <deric.walintukan@gmail.com>
2018-03-15 14:26:35 +0700

    Add pending tx snackbar

---
+++ src/components/PendingTxsSnackbar/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import { SnackbarContent } from 'material-ui/Snackbar';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import { FormattedMessage, injectIntl } from 'react-intl';
+
+import styles from './styles';
+
+class PendingTransactionsSnackbar extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    snackbarVisible: PropTypes.bool.isRequired,
+  };
+
+  render() {
+    const {
+      classes,
+      snackbarVisible,
+    } = this.props;
+
+    return (
+      <SnackbarContent
+        className={classes.snackbar}
+        open={snackbarVisible}
+        message={
+          <div>
+            Hello World
+          </div>
+        }
+      />
+    );
+  }
+}
+
+export default injectIntl(withStyles(styles, { withTheme: true })(PendingTransactionsSnackbar));
