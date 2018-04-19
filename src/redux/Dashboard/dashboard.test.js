40d9e95495f83432378a10ccce9ed9aaa34cc32e
alex-cory <results@alexcory.com>
2018-04-19 14:59:15 -0700

    added initial value to dashboard store `sortBy`, added a test for changing to DESC order

--- src/redux/Dashboard/dashboard.test.js
+++ src/redux/Dashboard/dashboard.test.js
@@ -1,14 +1,17 @@
 import { createStore } from '../store';
 import { SortBy } from '../../constants';
 import dashboardActions from './actions';
+const { Descending, Ascending } = SortBy;
 
 describe('Dashboard tests', () => {
   const store = createStore();
+
   it('should have the sortBy start out as ASC', () => {
-    expect(store.getState().dashboard.sortBy).toBe(SortBy.Ascending);
+    expect(store.getState().dashboard.sortBy).toBe(Ascending);
   });
+
   it('should change the sortBy to DESC when switching direction', () => {
-    store.dispatch(dashboardActions.sortOrderChanged(SortBy.Descending));
-    expect(store.getState().dashboard.sortBy).toBe(SortBy.Descending);
+    store.dispatch(dashboardActions.sortOrderChanged(Descending));
+    expect(store.getState().dashboard.sortBy).toBe(Descending);
   });
 });
