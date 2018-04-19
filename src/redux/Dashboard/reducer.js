bd0d92f87a89d59581250abaa0ebc1a00b5af521
alex-cory <results@alexcory.com>
2018-04-19 16:34:10 -0700

    removing dead code

--- src/redux/Dashboard/reducer.js
+++ src/redux/Dashboard/reducer.js
@@ -1,19 +1,12 @@
-import { Map } from 'immutable';
 import actions from './actions';
 import { SortBy } from '../../constants';
 
 const initState = {
-  value: undefined,
   sortBy: SortBy.Ascending,
 };
 
 export default function dashboardReducer(state = initState, action) {
   switch (action.type) {
-    case actions.TAB_INDEX_CHANGED:
-      return {
-        ...state,
-        tabIndex: action.value,
-      };
     case actions.SORT_ORDER_CHANGED:
       return {
         ...state,
