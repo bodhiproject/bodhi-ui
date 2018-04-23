80dfe3398e99e8a968d8a12227f548017dfb35dc
Deric Walintukan <deric.walintukan@gmail.com>
2018-04-23 13:58:09 -0700

    Add definitions

--- src/scenes/App/components/TermsAndConditions/index.jsx
+++ src/scenes/App/components/TermsAndConditions/index.jsx
@@ -44,7 +44,8 @@ export default class TermsAndConditions extends React.PureComponent {
           <Grid item xs={8}>
             <Paper className={classes.tncWrapper}>
               {this.renderTitle()}
-              {this.renderParagraph1()}
+              {this.renderGeneral()}
+              {this.renderDefinitions()}
             </Paper>
           </Grid>
           <Grid item xs></Grid>
@@ -54,20 +55,38 @@ export default class TermsAndConditions extends React.PureComponent {
   }
 
   renderTitle = () => (
-    <Typography variant="title" className={this.props.classes.title}>
+    <Typography variant="headline" className={this.props.classes.title}>
       <FormattedMessage id="tnc.title" defaultMessage="Bodhi Terms and Conditions" />
     </Typography>
   );
 
-  renderParagraph1 = () => (
+  renderGeneral = () => (
     <div>
-      <Typography variant="headline">
+      <Typography variant="title">
         <FormattedMessage id="tnc.general" defaultMessage="General" />
       </Typography>
       <ol>
         <li><FormattedMessage id="tnc.general1" defaultMessage="The Site is operated by the Bodhi Foundation (“Bodhi“), with registered address at 22 NORTH CANAL ROAD #02-00 SINGAPORE (048834). At this stage of the Platform development this Site is entirely for free, and no risking of any BOT or QTUM Tokens is required in order to use it. Therefore, all Events and/or Predictions made via the Site are entirely risk free, however this is subject to change in accordance with the development milestones in the White Paper." /></li>
         <li><FormattedMessage id="tnc.general2" defaultMessage="These terms and conditions (“Terms and Conditions“) govern your (“You“, “Your”, “Player“ or “Participant”) use of the platform provided to You by Bodhi.  These Terms and Conditions should be read carefully by You in their entirety prior to Your use of the Platform and/or Site. Please note that these Terms and Conditions constitute a legally binding agreement between You and the Company." /></li>
       </ol>
+    </div>
+  );
+
+  renderDefinitions = () => (
+    <div>
+      <Typography variant="title">
+        <FormattedMessage id="tnc.definitions" defaultMessage="Definitions" />
+      </Typography>
+      <FormattedMessage id="tnc.definitionsExplanation" defaultMessage="In these Terms and Conditions, the following words and phrases shall (unless the context otherwise requires) have the meanings set out beside them:" />
+      <ul>
+        <li><FormattedMessage id="tnc.definitions1" defaultMessage="“Event” shall mean a speculative question about the future that has a discrete and well-defined number of potential outcomes." /></li>
+        <li><FormattedMessage id="tnc.definitions2" defaultMessage="“Platform” shall mean the platform developed by Bodhi allowing for the creation of Events and making Predictions via the Site." /></li>
+        <li><FormattedMessage id="tnc.definitions3" defaultMessage="“Prediction” shall mean a prediction made via the Site in respect of an Event. " /></li>
+        <li><FormattedMessage id="tnc.definitions4" defaultMessage="“Restricted Territories” shall mean any jurisdiction in which creating Events making Predictions and/or the use of the platform is illegal as well as any additional jurisdictions added by Bodhi at its sole and absolute discretion." /></li>
+        <li><FormattedMessage id="tnc.definitions5" defaultMessage="“Site” shall mean any website and/or mobile site and/or mobile application owned, operated or hosted by the Company and through which the Platform is available." /></li>
+        <li><FormattedMessage id="tnc.definitions6" defaultMessage="“BOT” shall mean the utility token which is the Bodhi Token (BOT) cryptocurrency." /></li>
+        <li><FormattedMessage id="tnc.definitions7" defaultMessage="“We“, “Our“ or “Us” shall mean the Bodhi Foundation, and/or any subsidiaries, affiliates, employees, directors, officers, agents, suppliers, consultants and contractors." /></li>
+      </ul>
     </div>
   );
 }
