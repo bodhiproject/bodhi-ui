4a52bab7cc30e996dc7f544a19d05e1b3bf7b555
alex-cory <results@alexcory.com>
2018-05-01 16:19:54 -0700

    changed all .jsx -> js, updated EventCardsGridContainer and TxConfirmDialog, removed console warnings

--- src/components/TxConfirmDialog/index.js
+++ src/components/TxConfirmDialog/index.js
@@ -2,13 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import Dialog, { DialogTitle, DialogContent, DialogActions } from 'material-ui/Dialog';
-import { withStyles } from 'material-ui/styles';
 import { FormattedMessage, injectIntl, intlShape, defineMessages } from 'react-intl';
 
-import styles from './styles';
 import appActions from '../../redux/App/actions';
 
 const messages = defineMessages({
@@ -19,38 +16,22 @@ const messages = defineMessages({
 });
 
 @injectIntl
-@withStyles(styles, { withTheme: true })
 @connect((state) => ({
   txConfirmInfoAndCallback: state.App.get('txConfirmInfoAndCallback'),
 }), (dispatch) => ({
   clearTxConfirm: () => dispatch(appActions.clearTxConfirm()),
 }))
-
 export default class TxConfirmDialog extends Component {
   static propTypes = {
-    classes: PropTypes.object.isRequired,
     intl: intlShape.isRequired, // eslint-disable-line react/no-typos
     txConfirmInfoAndCallback: PropTypes.object.isRequired,
     clearTxConfirm: PropTypes.func.isRequired,
   }
 
   render() {
-    const {
-      intl: { formatMessage },
-      classes,
-      txConfirmInfoAndCallback,
-    } = this.props;
-
-    const {
-      txDesc,
-      txAmount,
-      txToken,
-      confirmCallback,
-    } = txConfirmInfoAndCallback;
-
-    const isOpen =
-      !_.isUndefined(txDesc) && !_.isUndefined(txAmount) &&
-      !_.isUndefined(txToken) && _.isFunction(confirmCallback);
+    const { intl: { formatMessage }, txConfirmInfoAndCallback } = this.props;
+    const { txDesc, txAmount, txToken, confirmCallback } = txConfirmInfoAndCallback;
+    const isOpen = !txDesc && !txAmount && !txToken && _.isFunction(confirmCallback);
 
     return (
       <Dialog open={isOpen}>
