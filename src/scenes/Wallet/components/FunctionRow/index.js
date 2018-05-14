31d8bf0c2fb6dbd11c8aaf2a7ac1d713f9c610c5
Bowen Zhang <bowen@bohdi.network>
2018-05-14 15:29:59 -0700

    make Tip components and make lines less

--- src/scenes/Wallet/components/FunctionRow/index.js
+++ src/scenes/Wallet/components/FunctionRow/index.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { injectIntl, FormattedMessage, intlShape, defineMessages } from 'react-intl';
-import { withStyles } from 'material-ui/styles';
-import Button from 'material-ui/Button';
-import Tooltip from 'material-ui/Tooltip';
-
+import { injectIntl, defineMessages } from 'react-intl';
+import { withStyles, Button, Tooltip } from 'material-ui';
 import EncryptDialog from '../EncryptDialog/index';
 import EncryptStatusDialog from '../EncryptStatusDialog/index';
 
@@ -26,6 +23,18 @@ const messages = defineMessages({
     id: 'str.restore',
     defaultMessage: 'Restores your backed up wallet data file. This will add all the addresses from the backed up wallet to your current wallet.',
   },
+  encryptButton: {
+    id: 'button.encrypt',
+    defaultMessage: 'Encrypt',
+  },
+  backupButton: {
+    id: 'button.backup',
+    defaultMessage: 'Backup',
+  },
+  restoreButton: {
+    id: 'button.restore',
+    defaultMessage: 'Restore',
+  },
 });
 
 @injectIntl
@@ -38,7 +47,6 @@ const messages = defineMessages({
 }))
 export default class FunctionRow extends Component {
   static propTypes = {
-    intl: intlShape.isRequired, // eslint-disable-line react/no-typos
     classes: PropTypes.object.isRequired,
     backupWallet: PropTypes.func.isRequired,
     importWallet: PropTypes.func.isRequired,
@@ -46,76 +54,42 @@ export default class FunctionRow extends Component {
       PropTypes.string,
       PropTypes.object,
     ]),
-  };
+  }
 
   static defaultProps = {
     encryptResult: undefined,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      encryptDialogVisible: false,
-    };
   }
+
+  state = {
+    encryptDialogVisible: false,
+  }
+
   render() {
-    const { classes, encryptResult, intl } = this.props;
+    const { classes, encryptResult } = this.props;
     const { encryptDialogVisible } = this.state;
     return (
       <div className={classes.functionRow}>
-        <Tooltip id="tooltip-icon" title={intl.formatMessage(messages.encrypt)}>
-          <Button variant="raised" color="primary" className={classes.button} onClick={this.onEncryptClicked}>
-            <FormattedMessage id="button.encrypt" defaultMessage="Encrypt" />
-          </Button>
-        </Tooltip>
-        <Tooltip id="tooltip-icon" title={intl.formatMessage(messages.backup)}>
-          <Button variant="raised" color="primary" className={classes.button} onClick={this.onBackup}>
-            <FormattedMessage id="button.backup" defaultMessage="Backup" />
-          </Button>
-        </Tooltip>
-        <Tooltip id="tooltip-icon" title={intl.formatMessage(messages.restore)}>
-          <Button variant="raised" color="primary" className={classes.button} onClick={this.onImport}>
-            <FormattedMessage id="button.resotre" defaultMessage="Restore" />
-          </Button>
-        </Tooltip>
+        <Tip onClick={() => { this.setState({ encryptDialogVisible: true }); }}>encrypt</Tip>
+        <Tip onClick={this.props.backupWallet}>backup</Tip>
+        <Tip onClick={this.props.importWallet}>restore</Tip>
         <EncryptDialog
           dialogVisible={encryptDialogVisible}
-          onClose={this.handleEncryptDialogClose}
+          onClose={() => { this.setState({ encryptDialogVisible: false }); }}
           onEncryptWallet={this.onEncryptWallet}
         />
         <EncryptStatusDialog
-          onClose={this.handleEncryptStatusDialogClose}
+          onClose={() => { this.setState({ encryptDialogVisible: false }); }}
           encryptResult={encryptResult}
         />
       </div>
     );
   }
-
-
-  onBackup = () => {
-    this.props.backupWallet();
-  }
-
-  onImport = () => {
-    this.props.importWallet();
-  }
-
-  onEncryptWallet = () => {
-    this.setState({
-      encryptDialogVisible: false,
-    });
-  }
-
-  onEncryptClicked = () => {
-    this.setState({
-      encryptDialogVisible: true,
-    });
-  }
-
-  handleEncryptDialogClose = () => {
-    this.setState({
-      encryptDialogVisible: false,
-    });
-  };
 }
+
+const Tip = injectIntl(withStyles(styles, { withTheme: true })(({ children, classes, intl, theme, ...props }) => (
+  <Tooltip id="tooltip-icon" title={intl.formatMessage(messages[children])}>
+    <Button variant="raised" color="primary" className={classes.button} {...props}>
+      {intl.formatMessage(messages[`${children}Button`])}
+    </Button>
+  </Tooltip>
+)));
--- src/scenes/Wallet/index.js
+++ src/scenes/Wallet/index.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { injectIntl } from 'react-intl';
-import { withStyles } from 'material-ui/styles';
 
 import MyBalances from './components/Balances/index';
 import FunctionRow from './components/FunctionRow/index';
 import WalletHistory from './components/History/index';
 import appActions from '../../redux/App/actions';
 import { AppLocation } from '../../constants';
-import styles from './styles';
 
-@injectIntl
-@withStyles(styles, { withTheme: true })
 @connect(null, (dispatch) => ({
   setAppLocation: (location) => dispatch(appActions.setAppLocation(location)),
 }))
