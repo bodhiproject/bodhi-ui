8e6a95ffb7f279b0a1a8776a2d7c211785769730
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-06 12:02:16 +0700

    Use content item comps

--- src/components/TutorialCarouselDialog/components/tutorial2.js
+++ src/components/TutorialCarouselDialog/components/tutorial2.js
@@ -34,21 +34,9 @@ const Tutorial2 = ({ classes }) => (
           </a>
         </Typography>
       </li>
-      <li>
-        <Typography variant="body2">
-          <FormattedMessage id="tutorial2.content1" defaultMessage="To encrypt your wallet, select “Launch Qtum Wallet” in the application menu and follow the instructions here." />
-        </Typography>
-      </li>
-      <li>
-        <Typography variant="body2">
-          <FormattedMessage id="tutorial2.content2" defaultMessage="Bitcoin wallets already contain roughly 100 different addresses assigned to one wallet. This is why you might see new addresses appearing in your wallet." />
-        </Typography>
-      </li>
-      <li>
-        <Typography variant="body2">
-          <FormattedMessage id="tutorial2.content3" defaultMessage="The Qtum Wallet is responsible for holding your passphrase and private keys. Bodhi Dapp does not store or handle this for you." />
-        </Typography>
-      </li>
+      <ContentItem id="tutorial2.content1" defaultMessage="To encrypt your wallet, select “Launch Qtum Wallet” in the application menu and follow the instructions here." />
+      <ContentItem id="tutorial2.content2" defaultMessage="Bitcoin wallets already contain roughly 100 different addresses assigned to one wallet. This is why you might see new addresses appearing in your wallet." />
+      <ContentItem id="tutorial2.content3" defaultMessage="The Qtum Wallet is responsible for holding your passphrase and private keys. Bodhi Dapp does not store or handle this for you." />
     </ul>
   </div>
 );
