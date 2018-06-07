68c0fe25d49cd4d65ea1da0891fd498c7110220f
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-07 17:43:32 +0700

    Use format message for override button

--- src/components/TutorialCarouselDialog/index.js
+++ src/components/TutorialCarouselDialog/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Button as _Button, withStyles } from 'material-ui';
 import Dialog, { DialogContent } from 'material-ui/Dialog';
 import cx from 'classnames';
-import { injectIntl, FormattedMessage } from 'react-intl';
+import { injectIntl, defineMessages } from 'react-intl';
 
 import styles from './styles';
 import Tutorial0 from './components/tutorial0';
@@ -13,6 +13,22 @@ import Tutorial3 from './components/tutorial3';
 import Tutorial4 from './components/tutorial4';
 import Tutorial5 from './components/tutorial5';
 import Tutorial6 from './components/tutorial6';
+
+const messages = defineMessages({
+  gotItLetsStart: {
+    id: 'tutorial.gotItLetsStart',
+    defaultMessage: 'Got It. Let\'s Start.',
+  },
+  next: {
+    id: 'tutorial.next',
+    defaultMessage: 'Next',
+  },
+  previous: {
+    id: 'tutorial.previous',
+    defaultMessage: 'Previous',
+  },
+});
+
 
 @injectIntl
 @withStyles(styles, { withTheme: true })
@@ -63,18 +79,15 @@ export default class TutorialCarouselDialog extends Component {
           <div className={classes.contentWrapper}>
             <CurrentComponentName />
             <div className={classes.buttonsWrapper}>
-              {currentIndex > 0 &&
-                <Button onClick={this.prevSlide}>
-                  <FormattedMessage id="tutorial.previous" defaultMessage="Previous" />
-                </Button>}
-              {currentIndex < this.components.length - 1 &&
-                <Button onClick={this.nextSlide}>
-                  <FormattedMessage id="tutorial.next" defaultMessage="Next" />
-                </Button>}
-              {currentIndex === this.components.length - 1 &&
-                <Button onClick={this.closeTutorial}>
-                  <FormattedMessage id="tutorial.gotItLetsStart" defaultMessage="Got It. Let's Start." />
-                </Button>}
+              {currentIndex > 0 && (
+                <Button onClick={this.prevSlide} msgId={messages.previous} />
+              )}
+              {currentIndex < this.components.length - 1 && (
+                <Button onClick={this.nextSlide} msgId={messages.next} />
+              )}
+              {currentIndex === this.components.length - 1 && (
+                <Button onClick={this.closeTutorial} msgId={messages.gotItLetsStart} />
+              )}
             </div>
           </div>
         </DialogContent>
@@ -83,11 +96,8 @@ export default class TutorialCarouselDialog extends Component {
   }
 }
 
-const Button = withStyles(styles)(({ classes, ...props }) => (
-  <_Button
-    {...props}
-    className={classes.button}
-    variant="raised"
-    size="medium"
-  />
-));
+const Button = injectIntl(withStyles(styles)(({ classes, intl, msgId, ...props }) => (
+  <_Button {...props} className={classes.button} variant="raised" size="medium">
+    {intl.formatMessage(msgId)}
+  </_Button>
+)));
