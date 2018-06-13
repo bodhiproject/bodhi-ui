5914db1be4b46b9d02e8fecc4193a509dbb0958a
Andy Chen <amazingandyyy@gmail.com>
2018-06-13 15:28:23 -0700

    update online icon

--- src/scenes/Activities/styles.js
+++ src/scenes/Activities/styles.js
@@ -5,12 +5,14 @@ const styles = (theme) => ({
     top: theme.sizes.navHeight,
     left: 0,
     right: 0,
+    height: '64px',
   },
   activitiesTabButton: {
     marginTop: '0px',
     marginBottom: '0px',
     paddingTop: '8px',
-    paddingBottom: '7px',
+    paddingBottom: '8px',
+    height: '64px',
   },
   activitiesTabContainer: {
     marginTop: theme.sizes.navHeight,
