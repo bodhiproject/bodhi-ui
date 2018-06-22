003b3e669415b0c42566c5b4bcc89bdfd837af62
Bowen Zhang <bowen@bohdi.network>
2018-06-22 13:28:13 -0700

    refactor encryptDialog to mobx

--- src/stores/WalletStore.js
+++ src/stores/WalletStore.js
@@ -1,6 +1,32 @@
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
+import axios from '../network/httpRequest';
+import Routes from '../network/routes';
 
 
 export default class WalletStore {
   @observable addresses = []
+  @observable walletEncrypted = false
+  @observable error = ''
+  @observable passphrase = ''
+
+  @action.bound
+  async encryptWallet(passphrase) {
+    try {
+      const { data: { result } } = await axios.post(Routes.api.encryptWallet, {
+        passphrase,
+      });
+      this.result = result;
+    } catch (error) {
+      const errorObject = {
+        route: Routes.api.encryptWallet,
+        message: error.message,
+      };
+      this.error = errorObject;
+    }
+  }
+
+  @action
+  onPassphraseChange = (passphrase) => {
+    this.passphrase = passphrase;
+  }
 }
