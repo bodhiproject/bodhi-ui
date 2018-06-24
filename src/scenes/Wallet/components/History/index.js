d8cad54fc8cb53acb8b5e6ade3d1d5b5bbbd69c2
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-24 16:30:14 +0700

    Change all components to standalone

--- src/scenes/Wallet/components/History/index.js
+++ src/scenes/Wallet/components/History/index.js
@@ -12,7 +12,7 @@ import {
   TableHead,
   TableRow,
   TableSortLabel,
-  TableFooter,
+  TableFooter as _TableFooter,
   TablePagination,
   Tooltip,
   withStyles,
@@ -48,116 +48,7 @@ export default class WalletHistory extends Component {
     txReturn: undefined,
   };
 
-  componentDidMount() {
-    this.props.store.walletHistory.queryTransactions();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { txReturn, syncBlockNum, store: { walletHistory: { queryTransactions } } } = this.props;
-
-    if ((txReturn && !nextProps.txReturn) || (syncBlockNum !== nextProps.syncBlockNum)) {
-      queryTransactions();
-    }
-  }
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Paper className={classes.txHistoryPaper}>
-        <Grid container spacing={0} className={classes.txHistoryGridContainer}>
-          <Typography variant="title">
-            <FormattedMessage id="walletHistory.transferHistory" defaultMessage="Transfer History" />
-          </Typography>
-          <Table className={classes.table}>
-            {this.getTableHeader()}
-            {this.getTableRows()}
-            {this.getTableFooter()}
-          </Table>
-        </Grid>
-      </Paper>
-    );
-  }
-
-  getTableHeader = () => {
-    const { orderBy, direction } = this.props.store.walletHistory;
-
-    const headerCols = [
-      {
-        id: 'createdTime',
-        name: 'str.time',
-        nameDefault: 'Time',
-        numeric: false,
-      },
-      {
-        id: 'senderAddress',
-        name: 'str.from',
-        nameDefault: 'From',
-        numeric: false,
-      },
-      {
-        id: 'receiverAddress',
-        name: 'str.to',
-        nameDefault: 'To',
-        numeric: false,
-      },
-      {
-        id: 'amount',
-        name: 'str.amount',
-        nameDefault: 'Amount',
-        numeric: true,
-      },
-      {
-        id: 'fee',
-        name: 'str.fee',
-        nameDefault: 'Gas Fee(QTUM)',
-        numeric: true,
-      },
-      {
-        id: 'status',
-        name: 'str.status',
-        nameDefault: 'Status',
-        numeric: false,
-      },
-      {
-        id: 'actions',
-        name: 'str.empty',
-        nameDefault: '',
-        numeric: false,
-        sortable: false,
-      },
-    ];
-
-    return (
-      <TableHead>
-        <TableRow>
-          {headerCols.map((column) => (
-            <TableCell
-              key={column.id}
-              numeric={column.numeric}
-              sortDirection={orderBy === column.id ? direction.toLowerCase() : false}
-            >
-              <Tooltip
-                title={<FormattedMessage id="str.sort" defaultMessage="Sort" />}
-                enterDelay={Config.intervals.tooltipDelay}
-                placement={column.numeric ? 'bottom-end' : 'bottom-start'}
-              >
-                <TableSortLabel
-                  active={orderBy === column.id}
-                  direction={direction.toLowerCase()}
-                  onClick={this.handleSorting(column.id)}
-                >
-                  <FormattedMessage id={column.name} default={column.nameDefault} />
-                </TableSortLabel>
-              </Tooltip>
-            </TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-    );
-  }
-
-  handleSorting = (property) => (event) => { // eslint-disable-line
+  onSortChange = (property) => (event) => { // eslint-disable-line
     const { walletHistory } = this.props.store;
 
     let order = SortBy.Descending.toLowerCase();
@@ -168,19 +59,7 @@ export default class WalletHistory extends Component {
     walletHistory.onSortingChange(property, order);
   }
 
-  getTableRows = () => {
-    const { store: { walletHistory: { list } } } = this.props;
-
-    return (
-      <TableBody>
-        {list.map((transaction, index) => (
-          this.getTableRow(transaction, index)
-        ))}
-      </TableBody>
-    );
-  }
-
-  handleClick = (id) => (event) => { // eslint-disable-line
+  onRowClick = (id) => (event) => { // eslint-disable-line
     const { expanded, onExpandedChange } = this.props.store.walletHistory;
 
     const expandedIndex = expanded.indexOf(id);
@@ -194,65 +73,193 @@ export default class WalletHistory extends Component {
     onExpandedChange(newExpanded);
   };
 
-  getTableRow = (transaction) => {
-    const { classes } = this.props;
-    const { expanded } = this.props.store.walletHistory;
-
-    const result = [];
-    const isExpanded = expanded.includes(transaction.txid);
-    result[0] = (
-      <TableRow key={transaction.txid} selected={isExpanded} onClick={this.handleClick(transaction.txid)} className={classes.clickToExpandRow}>
-        <TableCell className={classes.summaryRowCell}>
-          {getShortLocalDateTimeString(transaction.blockTime ? transaction.blockTime : transaction.createdTime)}
-        </TableCell>
-        <TableCell>
-          {transaction.senderAddress}
-        </TableCell>
-        <TableCell>
-          {transaction.receiverAddress}
-        </TableCell>
-        <TableCell numeric>
-          {`${transaction.amount || ''}  ${transaction.amount ? transaction.token : ''}`}
-        </TableCell>
-        <TableCell numeric>
-          {transaction.fee}
-        </TableCell>
-        <TableCell>
-          {transaction.status}
-        </TableCell>
-        <TableCell>
-          <i className={cx(isExpanded ? 'icon-ic_down' : 'icon-ic_up', 'icon iconfont', classes.arrowSize)} />
-        </TableCell>
-      </TableRow>
-    );
-    result[1] = (
-      <TableRow key={`txaddr-${transaction.txid}`} selected onClick={this.handleClick(transaction.txid)} className={isExpanded ? classes.show : classes.hide}>
-        <TransactionHistoryAddress transaction={transaction} />
-        <TableCell />
-        <TransactionHistoryID transaction={transaction} />
-        <TableCell /><TableCell /><TableCell /><TableCell />
-      </TableRow>
-    );
-
-    return result;
+  componentDidMount() {
+    this.props.store.walletHistory.queryTransactions();
   }
 
-  getTableFooter = () => {
-    const { fullList, perPage, page, onPageChange, onPerPageChange } = this.props.store.walletHistory;
+  componentWillReceiveProps(nextProps) {
+    const { txReturn, syncBlockNum, store: { walletHistory: { queryTransactions } } } = this.props;
+
+    if ((txReturn && !nextProps.txReturn) || (syncBlockNum !== nextProps.syncBlockNum)) {
+      queryTransactions();
+    }
+  }
+
+  render() {
+    const { classes, store: { walletHistory } } = this.props;
 
     return (
-      <TableFooter>
-        <TableRow>
-          <TablePagination
-            colSpan={12}
-            count={fullList.length}
-            rowsPerPage={perPage}
-            page={page}
-            onChangePage={(e, p) => onPageChange(p)}
-            onChangeRowsPerPage={(e) => onPerPageChange(e.target.value)}
-          />
-        </TableRow>
-      </TableFooter>
+      <Paper className={classes.txHistoryPaper}>
+        <Grid container spacing={0} className={classes.txHistoryGridContainer}>
+          <Typography variant="title">
+            <FormattedMessage id="walletHistory.transferHistory" defaultMessage="Transfer History" />
+          </Typography>
+          <Table className={classes.table}>
+            <TableHeader
+              orderBy={walletHistory.orderBy}
+              direction={walletHistory.direction}
+              onSortChange={this.onSortChange}
+            />
+            <TableRows
+              classes={classes}
+              list={walletHistory.list}
+              expanded={walletHistory.expanded}
+              onRowClick={this.onRowClick}
+            />
+            <TableFooter
+              fullList={walletHistory.fullList}
+              perPage={walletHistory.perPage}
+              page={walletHistory.page}
+              onPageChange={walletHistory.onPageChange}
+              onPerPageChange={walletHistory.onPerPageChange}
+            />
+          </Table>
+        </Grid>
+      </Paper>
     );
   }
 }
+
+const TableHeader = ({ orderBy, direction, onSortChange }) => {
+  const headerCols = [
+    {
+      id: 'createdTime',
+      name: 'str.time',
+      nameDefault: 'Time',
+      numeric: false,
+    },
+    {
+      id: 'senderAddress',
+      name: 'str.from',
+      nameDefault: 'From',
+      numeric: false,
+    },
+    {
+      id: 'receiverAddress',
+      name: 'str.to',
+      nameDefault: 'To',
+      numeric: false,
+    },
+    {
+      id: 'amount',
+      name: 'str.amount',
+      nameDefault: 'Amount',
+      numeric: true,
+    },
+    {
+      id: 'fee',
+      name: 'str.fee',
+      nameDefault: 'Gas Fee(QTUM)',
+      numeric: true,
+    },
+    {
+      id: 'status',
+      name: 'str.status',
+      nameDefault: 'Status',
+      numeric: false,
+    },
+    {
+      id: 'actions',
+      name: 'str.empty',
+      nameDefault: '',
+      numeric: false,
+      sortable: false,
+    },
+  ];
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headerCols.map((column) => (
+          <TableCell
+            key={column.id}
+            numeric={column.numeric}
+            sortDirection={orderBy === column.id ? direction.toLowerCase() : false}
+          >
+            <Tooltip
+              title={<FormattedMessage id="str.sort" defaultMessage="Sort" />}
+              enterDelay={Config.intervals.tooltipDelay}
+              placement={column.numeric ? 'bottom-end' : 'bottom-start'}
+            >
+              <TableSortLabel
+                active={orderBy === column.id}
+                direction={direction.toLowerCase()}
+                onClick={onSortChange(column.id)}
+              >
+                <FormattedMessage id={column.name} default={column.nameDefault} />
+              </TableSortLabel>
+            </Tooltip>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+const TableRows = ({ classes, list, expanded, onRowClick }) => (
+  <TableBody>
+    {list.map((transaction, index) => {
+      const rows = [];
+      const isExpanded = expanded.includes(transaction.txid);
+      rows[0] = (
+        <TableRow
+          key={transaction.txid}
+          selected={isExpanded}
+          onClick={onRowClick(transaction.txid)}
+          className={classes.clickToExpandRow}
+        >
+          <TableCell className={classes.summaryRowCell}>
+            {getShortLocalDateTimeString(transaction.blockTime ? transaction.blockTime : transaction.createdTime)}
+          </TableCell>
+          <TableCell>
+            {transaction.senderAddress}
+          </TableCell>
+          <TableCell>
+            {transaction.receiverAddress}
+          </TableCell>
+          <TableCell numeric>
+            {`${transaction.amount || ''}  ${transaction.amount ? transaction.token : ''}`}
+          </TableCell>
+          <TableCell numeric>
+            {transaction.fee}
+          </TableCell>
+          <TableCell>
+            {transaction.status}
+          </TableCell>
+          <TableCell>
+            <i className={cx(isExpanded ? 'icon-ic_down' : 'icon-ic_up', 'icon iconfont', classes.arrowSize)} />
+          </TableCell>
+        </TableRow>
+      );
+      rows[1] = (
+        <TableRow
+          key={`txaddr-${transaction.txid}`}
+          selected
+          onClick={onRowClick(transaction.txid)}
+          className={isExpanded ? classes.show : classes.hide}
+        >
+          <TransactionHistoryAddress transaction={transaction} />
+          <TableCell />
+          <TransactionHistoryID transaction={transaction} />
+          <TableCell /><TableCell /><TableCell /><TableCell />
+        </TableRow>
+      );
+      return rows;
+    })}
+  </TableBody>
+);
+
+const TableFooter = ({ fullList, perPage, page, onPageChange, onPerPageChange }) => (
+  <_TableFooter>
+    <TableRow>
+      <TablePagination
+        colSpan={12}
+        count={fullList.length}
+        rowsPerPage={perPage}
+        page={page}
+        onChangePage={(e, p) => onPageChange(p)}
+        onChangeRowsPerPage={(e) => onPerPageChange(e.target.value)}
+      />
+    </TableRow>
+  </_TableFooter>
+);
