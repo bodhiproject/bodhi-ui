9e6e8ba5f6a36e744b5a5ec08c89201304571ef1
Lysisos <kyan_0626@yahoo.co.jp>
2018-06-28 18:16:26 -0700

    #712 fix while not expanding tx as prop

--- src/scenes/Event/components/EventTxHistory/index.js
+++ src/scenes/Event/components/EventTxHistory/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import {
   Table,
   TableBody,
