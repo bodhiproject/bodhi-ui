38a1424a218f05370dbaba18ce6ea547ad27a7c0
Andy Chen <amazingandyyy@gmail.com>
2018-07-05 14:12:03 -0700

    update styles

---
+++ src/scenes/Activities/scenes/styles.js
@@ -0,0 +1,14 @@
+const styles = () => ({
+  hide: {
+    display: 'none',
+  },
+  show: {
+    display: 'table-row',
+  },
+  arrowSize: {
+    fontSize: '8px',
+    cursor: 'pointer',
+  },
+});
+
+export default styles;
