c17da2f6bb6309c21e33bcfd6f249e0351f11064
Andy Chen <amazingandyyy@gmail.com>
2018-07-09 18:38:05 -0700

    change BackBtn

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -31,47 +31,7 @@ export default class BackButton extends Component {
   }
 
   onBackClick = () => {
-<<<<<<< HEAD
     const { history, store: { ui: { location } } } = this.props;
     history.push(location);
-=======
-    const { history, store: { ui } } = this.props;
-
-    switch (ui.location) {
-      case AppLocation.ACTIVITY_HISTORY: {
-        history.push(AppLocation.ACTIVITY_HISTORY);
-        break;
-      }
-      case AppLocation.ALL_EVENTS: {
-        history.push(AppLocation.ALL_EVENTS);
-        break;
-      }
-      case AppLocation.QTUM_PREDICTION:
-      case AppLocation.bet: {
-        history.push(AppLocation.QTUM_PREDICTION);
-        break;
-      }
-      case AppLocation.BOT_COURT: {
-        history.push(AppLocation.BOT_COURT);
-        break;
-      }
-      case AppLocation.SET: {
-        history.push(AppLocation.SET);
-        break;
-      }
-      case AppLocation.FINALIZE: {
-        history.push(AppLocation.FINALIZE);
-        break;
-      }
-      case AppLocation.WITHDRAW: {
-        history.push(AppLocation.WITHDRAW);
-        break;
-      }
-      default: {
-        history.push(AppLocation.QTUM_PREDICTION);
-        break;
-      }
-    }
->>>>>>> change all, all testing passed
   };
 }
