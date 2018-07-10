529b184ea37436bc28436fe37833c324cb12750a
Andy Chen <amazingandyyy@gmail.com>
2018-07-09 18:33:44 -0700

    remove weird swarm

--- src/components/NavBar/components/NavLink/index.js
+++ src/components/NavBar/components/NavLink/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { Route } from 'react-router-dom';
-import { AppLocation } from 'constants';
 import { Link } from '../Link';
 
 
-const NavLink = observer(({ to, store: { ui }, ...props }) => {
-  const location = {
-    [AppLocation.bet]: AppLocation.QTUM_PREDICTION,
-    [AppLocation.BOT_COURT]: AppLocation.BOT_COURT,
-    [AppLocation.ALL_EVENTS]: AppLocation.ALL_EVENTS,
-    // so all the routes under /activities keep the pointer under it
-    [AppLocation.SET]: AppLocation.SET,
-    [AppLocation.FINALIZE]: AppLocation.SET,
-    [AppLocation.WITHDRAW]: AppLocation.SET,
-    [AppLocation.ACTIVITY_HISTORY]: AppLocation.SET,
-  }[ui.location];
-  return (
-    <Route exact path={to}>
-      {({ match }) => <Link to={to} active={!!match || to === location} {...props} />}
-    </Route>
-  );
-});
+const NavLink = observer(({ to, store: { ui: { location } }, ...props }) => (
+  <Route exact path={to}>
+    {({ match }) => <Link to={to} active={!!match || to === location} {...props} />}
+  </Route>
+));
 
 export default inject('store')(NavLink);
