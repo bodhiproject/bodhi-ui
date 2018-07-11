7cfb6ae380f27f0806fe48865c45228402ef08f0
alex-cory <results@alexcory.com>
2018-07-11 15:13:11 -0700

    removing

--- src/scenes/Activities/index.js
+++ src/scenes/Activities/index.js
@@ -4,7 +4,6 @@ import { inject, observer } from 'mobx-react';
 import { Tabs, Tab, withStyles } from '@material-ui/core';
 import { injectIntl, intlShape, defineMessages } from 'react-intl';
 import { Routes, EventStatus } from 'constants';
-import _ from 'lodash';
 
 import ResultSetting from './scenes/ResultSetting';
 import Finalize from './scenes/Finalize';
