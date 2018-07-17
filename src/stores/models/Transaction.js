f06913c81f0d1e71845af13367fe3f089f4d919e
alex-cory <results@alexcory.com>
2018-07-17 15:02:21 -0700

    invalid option in transactions table on bet oracle page fixed

--- src/stores/models/Transaction.js
+++ src/stores/models/Transaction.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 import { Token, TransactionType } from 'constants';
 import { gasToQtum, satoshiToDecimal } from '../../helpers/utility';
+const { APPROVE_CREATE_EVENT, APPROVE_SET_RESULT, APPROVE_VOTE, BET, VOTE, SET_RESULT, FINALIZE_RESULT } = TransactionType;
 
 export default class Transaction {
   type = ''
@@ -29,9 +30,12 @@ export default class Transaction {
     this.gasLimit = Number(this.gasLimit);
     this.gasPrice = Number(this.gasPrice);
     this.fee = gasToQtum(this.gasUsed);
+    const { topic, type, optionIdx } = transaction;
+    if (topic && [APPROVE_SET_RESULT, APPROVE_VOTE, BET, VOTE, SET_RESULT, FINALIZE_RESULT].includes(type)) {
+      this.name = topic.options[optionIdx];
+    }
 
     if (this.token === Token.BOT) {
-      const { APPROVE_CREATE_EVENT, APPROVE_SET_RESULT, APPROVE_VOTE } = TransactionType;
       if (_.includes([APPROVE_CREATE_EVENT, APPROVE_SET_RESULT, APPROVE_VOTE], this.type)) {
         // Don't show the amount for any approves
         this.amount = undefined;
