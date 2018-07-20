f77c48aa8d6c6db3f64e2d2c9c65dcade80eab7c
alex-cory <results@alexcory.com>
2018-07-20 14:44:03 -0700

    removed comments in ImportantNote and added babel-styled-components for better debugging in react chrome devtools

--- src/scenes/Event/scenes/Oracle/components/Content.js
+++ src/scenes/Event/scenes/Oracle/components/Content.js
@@ -4,5 +4,6 @@ import { Grid } from '@material-ui/core';
 
 export const Content = styled(Grid).attrs({ item: true, xs: 12, md: 8 })`
   padding: ${props => props.theme.padding.lg.px};
+  padding-right: ;
   overflow-x: hidden;
 `;
