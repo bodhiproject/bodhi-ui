3e2fa2236ac6dbeba3a75d8e0a2f523d0dd226b2
alex-cory <results@alexcory.com>
2018-07-20 14:53:22 -0700

    making it more clear what is old

--- src/scenes/App/index.js
+++ src/scenes/App/index.js
@@ -9,10 +9,10 @@ import BottomBar from '../../components/BottomBar';
 import CreateEvent from '../CreateEvent';
 import PendingTransactionsSnackbar from '../../components/PendingTransactionsSnackbar';
 import GlobalSnackbar from '../../components/GlobalSnackbar';
-import TxSentDialog from '../../components/TxSentDialog/old';
+import OLDTxSentDialog from '../../components/TxSentDialog/old';
 import WalletUnlockDialog from '../../components/WalletUnlockDialog';
 import ErrorDialog from '../../components/ErrorDialog';
-import TxConfirmDialog from '../../components/TxConfirmDialog/old';
+import OLDTxConfirmDialog from '../../components/TxConfirmDialog/old';
 import TutorialCarouselDialog from '../../components/TutorialCarouselDialog';
 import Loader from './components/Loader';
 
@@ -30,10 +30,10 @@ const App = observer(({ classes, match: { url }, store }) => (
         <CreateEvent />
         <PendingTransactionsSnackbar />
         <GlobalSnackbar />
-        <TxSentDialog />
+        <OLDTxSentDialog />
         <WalletUnlockDialog />
         <ErrorDialog />
-        <TxConfirmDialog txDesc="" txAmount={10} txToken="QTUM" />
+        <OLDTxConfirmDialog txDesc="" txAmount={10} txToken="QTUM" />
         <TutorialCarouselDialog />
       </Fragment>
     )}
