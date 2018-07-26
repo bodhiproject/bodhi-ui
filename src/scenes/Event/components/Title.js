f8531f9fb0416e733bf3ca85c64db5c4008b99c3
alex-cory <results@alexcory.com>
2018-07-26 13:57:16 -0700

    removing accidental file changes from another branch

--- src/scenes/Event/components/Title.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { Typography } from '@material-ui/core';
-
-
-export const Title = styled(Typography).attrs({ variant: 'display1' })`
-  margin-bottom: ${props => props.theme.padding.md.px} !important;
-`;
-
--- src/scenes/Event/components/index.js
+++
@@ -1,7 +0,0 @@
-export * from './Button';
-export * from './Content';
-export * from './Row';
-export * from './SidebarContainer';
-export * from './Title';
-export * from './OracleTxConfirmDialog';
-export { default as Option } from './Option';
