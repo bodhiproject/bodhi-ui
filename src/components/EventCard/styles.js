a03a467b5a3ab11fb0a1bcbfe5c15b55d10ae3a0
Andy Chen <amazingandyyy@gmail.com>
2018-08-13 16:25:35 -0700

    braodcast new style to old cards

--- src/components/EventCard/styles.js
+++ src/components/EventCard/styles.js
@@ -1,6 +1,17 @@
 const styles = (theme) => ({
   eventCard: {
     position: 'relative',
+    borderRadius: '5px',
+    boxShadow: '0px 0px 20px 0px rgba(0,0,0,0.05)',
+    border: '2px solid rgba(0,0,0,0.075)',
+    '&:hover': {
+      boxShadow: '0px 5px 20px 3px rgba(0,0,0,0.1)',
+      transform: 'translateY(-2px)',
+      transition: '.1s all ease-in-out',
+    },
+    '&:active': {
+      opacity: '0.9',
+    },
   },
   eventCardSection: {
     position: 'relative',
@@ -19,17 +30,22 @@ const styles = (theme) => ({
   },
   dashboardTime: {
     color: theme.palette.text.hint,
+    fontWeight: '600',
+    display: 'block',
+    paddingBottom: '10px',
   },
   eventCardName: {
     marginBottom: theme.padding.xs.px,
     display: '-webkit-box',
-    maxWidth: '400px',
+    maxWidth: '700px',
     maxHeight: '160px',
     margin: '0 auto',
     WebkitLineClamp: 5,
     WebkitBoxOrient: 'vertical',
     overflow: 'hidden',
     textOverflow: 'ellipsis',
+    fontWeight: '700',
+    color: 'rgba(0,0,0,0.75)',
   },
   unconfirmedTag: {
     background: theme.palette.secondary.light,
@@ -48,7 +64,9 @@ const styles = (theme) => ({
     bottom: theme.padding.sm.px,
     color: theme.palette.text.primary,
   },
-  eventCardBg: {},
+  eventCardInfoItem: {
+    padding: '3px 0px',
+  },
 });
 
 export default styles;
