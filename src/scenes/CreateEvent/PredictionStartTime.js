0277cba1c8735352bb4e2077b03a89a5b38989a8
Lysisos <kyan_0626@yahoo.co.jp>
2018-08-13 18:00:41 -0700

    Picker mixed

--- src/scenes/CreateEvent/PredictionStartTime.js
+++ src/scenes/CreateEvent/PredictionStartTime.js
@@ -13,7 +13,6 @@ const messages = defineMessages({
 const PredictionStartTime = observer(({ store: { createEvent } }) => (
   <Section title={messages.createBetStartTimeMsg}>
     <DateRow
-      isOpen
       error={createEvent.error.prediction.startTime}
       onChange={e => createEvent.prediction.startTime = e.target.value}
       value={createEvent.prediction.startTime}
