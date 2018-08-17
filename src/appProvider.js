f1d639957b208a158b5530fed3699a6f77dd9cc4
Lysisos <kyan_0626@yahoo.co.jp>
2018-08-16 17:34:12 -0700

    Remove Redux?

--- src/appProvider.js
+++ src/appProvider.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { IntlProvider } from 'react-intl';
 import { MuiThemeProvider } from '@material-ui/core';
-import { Provider } from 'react-redux';
 import { Route, BrowserRouter } from 'react-router-dom';
 import { Provider as MobxProvider, observer } from 'mobx-react';
 import { ThemeProvider } from 'styled-components';
@@ -16,28 +15,25 @@ import '../src/style/styles.less';
 
 
 /**
- * store === redux store
  * xstore === mobx store
  */
-export const AppProvider = observer(({ xstore, store }) => (
+export const AppProvider = observer(({ xstore }) => (
   <ThemeProvider theme={styledTheme}>
-    <Provider store={store}>
-      <MobxProvider store={xstore}>
-        <MuiThemeProvider theme={bodhiTheme}>
-          <IntlProvider locale={xstore.ui.locale} messages={xstore.ui.localeMessages}>
-            <ApolloProvider client={graphClient}>
-              <BrowserRouter>
-                <MuiPickersUtilsProvider utils={MomentUtils}>
-                  <Route
-                    path="/"
-                    render={(props) => <App match={props.match} />}
-                  />
-                </MuiPickersUtilsProvider>
-              </BrowserRouter>
-            </ApolloProvider>
-          </IntlProvider>
-        </MuiThemeProvider>
-      </MobxProvider>
-    </Provider>
+    <MobxProvider store={xstore}>
+      <MuiThemeProvider theme={bodhiTheme}>
+        <IntlProvider locale={xstore.ui.locale} messages={xstore.ui.localeMessages}>
+          <ApolloProvider client={graphClient}>
+            <BrowserRouter>
+              <MuiPickersUtilsProvider utils={MomentUtils}>
+                <Route
+                  path="/"
+                  render={(props) => <App match={props.match} />}
+                />
+              </MuiPickersUtilsProvider>
+            </BrowserRouter>
+          </ApolloProvider>
+        </IntlProvider>
+      </MuiThemeProvider>
+    </MobxProvider>
   </ThemeProvider>
 ));
