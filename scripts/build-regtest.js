398d241c27130b83206df50723b4c763233ba53a
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-05 10:58:41 +0700

    Use env vars in routes

--- scripts/build-regtest.js
+++ scripts/build-regtest.js
@@ -1,6 +1,6 @@
 // Set regtest env vars
 process.env.API_HOSTNAME = 'test.puti.io';
-process.env.API_PORT = 6666;
+process.env.API_PORT = 5555;
 
 // Run build script
 require('./build');
--- src/network/routes.js
+++ src/network/routes.js
@@ -1,11 +1,19 @@
-import { isProduction } from '../config/app';
+/* eslint-disable no-unused-vars */
+const HOSTNAME_MAINNET = 'puti.io';
+const HOSTNAME_TESTNET = 'dev.puti.io';
+const HOSTNAME_REGTEST = 'test.puti.io';
 
-const PORT = isProduction() ? 8989 : 6767;
-const AUTHORITY = `${isProduction() ? 'puti.io' : 'localhost'}:${PORT}`;
-const HTTP_ROUTE = `${isProduction() ? 'https' : 'http'}://${AUTHORITY}`;
-const WS_ROUTE = `${isProduction() ? 'wss' : 'ws'}://${AUTHORITY}/graphql`;
+const PORT_MAINNET = 8989;
+const PORT_TESTNET = 6767;
+const PORT_REGTEST = 5555;
+/* eslint-enable no-unused-vars */
 
-const BASE_QTUM_WEB = `https://${isProduction() ? 'explorer' : 'testnet'}.qtum.org`;
+const PORT = process.env.API_PORT ? Number(process.env.API_PORT) : PORT_REGTEST;
+const HOSTNAME = process.env.API_HOSTNAME ? process.env.API_HOSTNAME : HOSTNAME_REGTEST;
+const HTTP_ROUTE = `https://${HOSTNAME}:${PORT}`;
+const WS_ROUTE = `wss://${HOSTNAME}:${PORT}/graphql`;
+
+const BASE_QTUM_WEB = `https://${HOSTNAME === HOSTNAME_MAINNET ? 'explorer' : 'testnet'}.qtum.org`;
 const BASE_INSIGHT = `${BASE_QTUM_WEB}/insight-api`;
 
 export default {
