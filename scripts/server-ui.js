e5aacd2cb9dd9d0b79fe8ce62af419bf3bb2e86c
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-07 13:47:42 +0700

    Console log out

--- scripts/server-ui.js
+++ scripts/server-ui.js
@@ -1,10 +1,22 @@
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
+
+const pipeLogs = (process) => {
+  process.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+  process.stderr.on('data', (data) => {
+    console.log(data.toString());
+  });
+};
 
 console.log('Building mainnet UI...');
-exec('npm run build:mainnet --output=/var/www/bodhi/mainnet');
+let process = execSync('npm run build:mainnet --output=/var/www/bodhi/mainnet');
+pipeLogs(process);
 
 console.log('Building testnet UI...');
-exec('npm run build:testnet --output=/var/www/bodhi/testnet');
+let process = execSync('npm run build:testnet --output=/var/www/bodhi/testnet');
+pipeLogs(process);
 
 console.log('Building regtets UI...');
-exec('npm run build:regtest --output=/var/www/bodhi/regtest');
+let process = execSync('npm run build:regtest --output=/var/www/bodhi/regtest');
+pipeLogs(process);
