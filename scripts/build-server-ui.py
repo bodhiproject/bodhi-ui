df465deadc27d498771e49f4a99e68e0a737159a
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-07 14:27:26 +0700

    Add python script

---
+++ scripts/build-server-ui.py
@@ -0,0 +1,13 @@
+from subprocess import call
+
+def buildEnv(env):
+    cmd = 'build:%s' % env
+    path = '/var/www/bodhi/' + env
+    output = '-output=%s' % path
+
+    print 'Building %s UI at %s' % env % path
+    call(['sudo', 'npm run', cmd, output])
+
+buildEnv('mainnet')
+buildEnv('testnet')
+buildEnv('regtest')
