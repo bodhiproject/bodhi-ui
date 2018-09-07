3f832df18b4fa21e81ea54578b5b6cb7b8f87670
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-07 14:09:15 +0700

    Change params

--- scripts/server-ui.js
+++ scripts/server-ui.js
@@ -1,14 +1,11 @@
 const { execSync } = require('child_process');
 
 const runProcess = (env) => {
-  const cmd = `npm run build:${env}`;
   const path = `/var/www/bodhi/${env}`;
-
   console.log(`Building ${env} UI at ${path}`);
-  execSync(cmd, [`--output=${path}`], function(err, stdout, stderr) {
-    if (stdout) console.log(stdout);
-    if (stderr) console.error(stderr);
-    if (err) console.error(err);
+  execSync(`npm run build:${env} --output=${path}`, {
+    stdio: 'inherit',
+    shell: true,
   });
 };
 
