d5e6307d1a78aea5bdf3819a92c03ef65fdbfae3
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-07 14:30:20 +0700

    Fix multiple string placeholder

--- scripts/build-server-ui.py
+++ scripts/build-server-ui.py
@@ -5,7 +5,7 @@ def buildEnv(env):
     path = '/var/www/bodhi/' + env
     output = '-output=%s' % path
 
-    print 'Building %s UI at %s' % env path
+    print 'Building %s UI at %s' % (env, path)
     call(['sudo', 'npm run', cmd, output])
 
 buildEnv('mainnet')
