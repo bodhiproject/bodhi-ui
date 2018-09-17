67d369836a1fb504bd4863fcdba194bae1c9c81f
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-17 13:35:01 +0700

    Export all models

--- src/stores/models/index.js
+++ src/stores/models/index.js
@@ -1,6 +1,7 @@
-export { default as WalletAddress } from './WalletAddress';
 export { default as Option } from './Option';
 export { default as Oracle } from './Oracle';
-export { default as Transaction } from './Transaction';
+export { default as SyncInfo } from './SyncInfo';
 export { default as Topic } from './Topic';
+export { default as Transaction } from './Transaction';
 export { default as TransactionCost } from './TransactionCost';
+export { default as WalletAddress } from './WalletAddress';
