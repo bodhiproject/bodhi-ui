dd8ebd559c83cc9b885fc1dc5c63758ea6e21f13
zhangtao <zhangtao@yunfutech.com>
2018-09-17 21:07:27 +0800

    QtumPrediction responsive

--- src/scenes/QtumPrediction/components/TopActions/styles.js
+++ src/scenes/QtumPrediction/components/TopActions/styles.js
@@ -5,6 +5,14 @@ const styles = (theme) => ({
   createEventButton: {
     padding: `12px ${theme.padding.sm.px}`,
     verticalAlign: 'middle',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 12,
+    },
+  },
+  createEventButtonIcon: {
+    [theme.breakpoints.down('xs')]: {
+      fontSize: theme.sizes.font.textMd,
+    },
   },
   dashboardActionsRight: {
     textAlign: 'right',
@@ -13,9 +21,26 @@ const styles = (theme) => ({
     display: 'inline-block',
     padding: `${theme.padding.unit.px} ${theme.padding.sm.px}`,
     verticalAlign: 'middle',
+    [theme.breakpoints.down('xs')]: {
+      padding: `${theme.padding.unit.px} ${theme.padding.unit.px}`,
+    },
   },
   dashboardActionsSortLabel: {
     marginRight: theme.padding.sm.px,
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 12,
+      marginRight: theme.padding.unit.px,
+    },
+  },
+  dashboardActionsSelect: {
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 12,
+    },
+  },
+  dashboardActionsMenuItem: {
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 12,
+    },
   },
 });
 
