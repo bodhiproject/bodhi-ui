556f5f21757fea3f8bd187f46cee22a803789404
Zhaoyang Han <bernardhan221@gmail.com>
2018-09-17 12:07:40 -0700

    model option in progress

--- src/stores/models/Option.test.js
+++ src/stores/models/Option.test.js
@@ -1,3 +1,5 @@
+import { observable } from 'mobx';
+import { Token } from 'constants';
 import Option from './Option';
 const localStorageMock = {
   getItem: jest.fn(),
@@ -8,30 +10,37 @@ const navigatorMock = {};
 global.navigator = navigatorMock;
 global.localStorage = localStorageMock;
 
+const { QTUM, BOT } = Token;
+
 describe('stores/models/Option', () => {
   const addr = {
     address: 'qSu4uU8MGp2Ya6j9kQZAtizUfC82aCvGT1',
     qtum: 2000,
     bot: 100,
   };
-  const app = {
+  const app = observable({
     sortBy: 'ASC',
     wallet: { addresses: [addr] },
     global: { syncBlockNum: 0 },
     refreshing: false,
     ui: { location: 0 },
-  }; // mock the appstore
-  const oracle = {
-    txid: 0,
-    amounts: [],
-    consensusThreshold: 100,
-    address: '02e91962156da21fae38e65038279c020347e4ff',
-    topicAddress: '4044f951857f2885d66d29a475235dacdaddea84',
-    resultSetEndTime: 10,
-    endTime: 20,
-    options: [],
-  };
+  }); // mock the appstore
   let store;
-  // beforeEach(() => store = new Option());
-  it('haha', () => expect(1).toBe(1));
+  beforeEach(() => store = null);
+  it('Constructor 1', () => {
+    const name = 'cons test 1';
+    const i = 0;
+    const oracle = {
+      txid: 0,
+      amounts: [],
+      consensusThreshold: 100,
+      address: '02e91962156da21fae38e65038279c020347e4ff',
+      topicAddress: '4044f951857f2885d66d29a475235dacdaddea84',
+      resultSetEndTime: 10,
+      endTime: 20,
+      options: [],
+      token: QTUM,
+    };
+    store = new Option(name, i, oracle, app);
+  });
 });
