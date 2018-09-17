7b7c2c8c030d7c24cb53e40ec84dcf5b05b71ad1
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-17 16:58:26 +0700

    Add wallet selector and table

--- src/components/ExecuteTxDialog/store.js
+++ src/components/ExecuteTxDialog/store.js
@@ -1,11 +1,15 @@
 import { observable } from 'mobx';
 
+import { WalletProvider } from '../../constants';
+
 const INIT_VALUES = {
   visible: false,
-  provider: 'qrypto',
+  provider: WalletProvider.QRYPTO,
+  txFees: [],
 };
 
 export default class ExecuteTxDialogStore {
   @observable visible = INIT_VALUES.visible;
   @observable provider = INIT_VALUES.provider;
+  @observable txFees = INIT_VALUES.txFees;
 }
