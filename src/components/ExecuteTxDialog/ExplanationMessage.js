748ec327d3803ea9870c7e7197e08195d0be91b5
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-20 10:24:20 +0700

    Change text for execute tx msg

--- src/components/ExecuteTxDialog/ExplanationMessage.js
+++ src/components/ExecuteTxDialog/ExplanationMessage.js
@@ -7,16 +7,15 @@ import { sumBy } from 'lodash';
 import styles from './styles';
 import { getTxTypeFormatted } from '../../helpers/utility';
 
-const ExplanationMessage = ({ classes, intl, store: { tx: { fees, type, option, amount, token } } }) => {
+const ExplanationMessage = ({ classes, intl, store: { tx: { fees, type } } }) => {
   const txAction = getTxTypeFormatted(type, intl);
-  const txDesc = option ? `${txAction} on ${option.name}` : txAction;
   const txFee = sumBy(fees, ({ gasCost }) => gasCost ? parseFloat(gasCost) : 0);
   return (
     <div className={classes.explanationMsgContainer}>
       <FormattedMessage
-        id='txConfirm.messageWithFee'
-        defaultMessage='You are about to {txDesc} for {txAmount} {txToken} with a maximum transaction fee of {txFee} QTUM. Any unused transaction fees will be refunded to you. Please click OK to continue.'
-        values={{ txDesc, txAmount: amount, txToken: token, txFee }}
+        id='txConfirm.txFeeMsg'
+        defaultMessage='You are about to {txAction} with a maximum transaction fee of {txFee} QTUM. Any unused transaction fees will be refunded to you.'
+        values={{ txAction, txFee }}
       />
     </div>
   );
