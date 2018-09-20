31b612907f0c814e3b0d023e28f005c1485ca74d
Lysisos <kyan_0626@yahoo.co.jp>
2018-09-20 14:45:24 -0700

    AllEventStore Test Fix

--- src/stores/AllEventsStore.test.js
+++ src/stores/AllEventsStore.test.js
@@ -34,7 +34,7 @@ describe('AllEventsStore', () => {
     store = new AllEventsStore(app); // create a new instance before each test case
     mockResetOracleList();
     mockResetTopicList();
-    for (let i = 0; i < 24; i++) {
+    for (let i = 0; i < 18; i++) {
       mockAddTopic({ txid: i });
       mockAddOracle({
         txid: i,
@@ -52,8 +52,8 @@ describe('AllEventsStore', () => {
   /** all following test cases target specifically to the mock backend, eg. network/graphql/__mocks__/queries.js */
   it('Init', async () => {
     const curListNum = store.list.length;
-    expect(curListNum).toBe(0);
     expect.assertions(4);
+    expect(curListNum).toBe(0);
     await store.init();
     expect(app.ui.location).toBe(Routes.ALL_EVENTS);
     expect(store.list.length).toBe(curListNum + store.limit);
@@ -63,31 +63,28 @@ describe('AllEventsStore', () => {
   it('Load More Events', async () => {
     await store.init();
     const curSkip = store.skip;
-    const curListNum = store.list.length;
     expect.assertions(6);
     expect(store.list.length).toBe(store.limit);
     expect(store.loadingMore).toBe(false);
     await store.loadMoreEvents();
-    expect(store.list.length).toBe((curListNum + store.limit) - 2); // mock backend only has 46 events in total
+    expect(store.list.length).toBe(36); // mock backend only has 46 events in total
     expect(store.loadingMore).toBe(false);
     expect(store.hasMore).toBe(false); // no events left in backend
     expect(store.skip).toBe(curSkip + store.limit);
   });
 
-  it('Reaction SortBy', () => {
-    expect.assertions(3);
+  it('Reaction SortBy', async () => {
+    expect.assertions(4);
     expect(store.list.length).toBe(0);
     app.ui.location = Routes.ALL_EVENTS;
     app.sortBy = 'Yo';
-    const a = queryAllOracles(null, null, 100, 0);
-    sleep(500);
-    expect(a.length).toBe(store.limit);
-    expect(store.app.location).toBe(Routes.ALL_EVENTS);
+    await sleep(100);
+    expect(store.app.ui.location).toBe(Routes.ALL_EVENTS);
     expect(store.app.sortBy).toBe('Yo');
     expect(store.list.length).toBe(store.limit);
   });
 
-  it('Reaction Wallet Addr', () => {
+  it('Reaction Wallet Addr', async () => {
     expect.assertions(2);
     expect(store.list.length).toBe(0);
     app.ui.location = Routes.ALL_EVENTS;
@@ -97,25 +94,25 @@ describe('AllEventsStore', () => {
       bot: 10,
     };
     app.wallet.addresses.push(newAddr);
-    sleep(500);
+    await sleep(100);
     expect(store.list.length).toBe(store.limit);
   });
 
-  it('Reaction BlockNum', () => {
+  it('Reaction BlockNum', async () => {
     expect.assertions(2);
     expect(store.list.length).toBe(0);
     app.ui.location = Routes.ALL_EVENTS;
     app.global.syncBlockNum = 1;
-    sleep(500);
+    await sleep(100);
     expect(store.list.length).toBe(store.limit);
   });
 
-  it('Reaction Refreshing', () => {
+  it('Reaction Refreshing', async () => {
     expect.assertions(2);
     expect(store.list.length).toBe(0);
     app.ui.location = Routes.ALL_EVENTS;
     app.refreshing = true;
-    sleep(500);
+    await sleep(100);
     expect(store.list.length).toBe(store.limit);
   });
 });
