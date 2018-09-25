7335002ad5e3299a5680ad32ede6c0b0e14d15c1
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-25 21:09:00 +0700

    Fix padding for important note

--- src/components/ImportantNote/index.js
+++ src/components/ImportantNote/index.js
@@ -1,36 +1,15 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-import { withStyles } from '@material-ui/core';
+import { withStyles, Typography } from '@material-ui/core';
+import cx from 'classnames';
 
 import styles from './styles';
 
 export const ImportantNote = withStyles(styles)(({ classes, heading, message, ...props }) => !!(heading && message) && (
-  <Wrapper {...props}>
-    <Icon />
-    <Heading>{heading}</Heading>
-    <Message className={classes.messageText}>{message}</Message>
-  </Wrapper>
+  <div {...props}>
+    <div className={classes.iconHeadingContainer}>
+      <i className={cx('icon iconfont icon-ic_info', classes.icon)} />
+      <Typography className={classes.heading}>{heading}</Typography>
+    </div>
+    <Typography className={classes.message}>{message}</Typography>
+  </div>
 ));
-
-const Wrapper = styled.div`
-  ${({ theme: { padding } }) => css`
-    margin: ${padding.xs.px} ${padding.xs.px} 0px ${padding.xs.px};
-  `}
-`;
-
-const Heading = styled.span`
-  font-size: ${props => props.theme.sizes.font.textMd};
-  font-weight: ${props => props.theme.typography.fontWeightBold};
-  color: ${props => props.theme.palette.text.primary};
-`;
-
-const Message = styled.p`
-  font-size: ${props => props.theme.sizes.font.textSm};
-  color: ${props => props.theme.palette.text.secondary};
-`;
-
-const Icon = styled.i.attrs({ className: 'icon iconfont icon-ic_info' })`
-  font-size: ${props => props.theme.sizes.font.textMd};
-  color: ${props => props.theme.palette.primary.main};
-  margin-right: ${props => props.theme.padding.unit.px};
-`;
