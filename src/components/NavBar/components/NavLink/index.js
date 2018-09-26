a1f78e53794209b01b83dd0b2eb05d2dfa911dc2
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-26 16:44:50 +0700

    Fix arrow not showing for sub tabs in activity history

--- src/components/NavBar/components/NavLink/index.js
+++ src/components/NavBar/components/NavLink/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { Route } from 'react-router-dom';
+import { includes } from 'lodash';
+
 import { Link } from '../Link';
 
 
-const NavLink = observer(({ to, store: { ui: { location } }, ...props }) => (
+const NavLink = ({ to, routes, store: { ui: { location } }, ...props }) => (
   <Route exact path={to}>
-    {({ match }) => <Link to={to} active={!!match || to === location} {...props} />}
+    {() => <Link to={to} active={(routes && includes(routes, location)) || to === location} {...props} />}
   </Route>
-));
+);
 
-export default inject('store')(NavLink);
+export default inject('store')(observer(NavLink));
