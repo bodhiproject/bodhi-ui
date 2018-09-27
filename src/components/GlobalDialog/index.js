369063298cd58e8431524506e39b1939416a0a4d
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-27 17:35:11 +0700

    Use define msgs for global dialog

--- src/components/GlobalDialog/index.js
+++ src/components/GlobalDialog/index.js
@@ -3,9 +3,28 @@ import PropTypes from 'prop-types';
 import { inject, observer } from 'mobx-react';
 import { withStyles, Typography, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
 import { Error } from '@material-ui/icons';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { FormattedMessage, injectIntl, defineMessages } from 'react-intl';
 
 import styles from './styles';
+
+const messages = defineMessages({
+  'str.error': {
+    id: 'str.error',
+    defaultMessage: 'Error',
+  },
+  'qrypto.loggedIntoWrongNetwork': {
+    id: 'qrypto.loggedIntoWrongNetwork',
+    defaultMessage: 'Logged into wrong network. Switch your Qrypto network.',
+  },
+  'qrypto.loginToView': {
+    id: 'qrypto.loginToView',
+    defaultMessage: 'Please login to Qrypto to view this page.',
+  },
+  'create.pendingExists': {
+    id: 'create.pendingExists',
+    defaultMessage: 'You can only create 1 event at a time. Please wait until your other Event is created.',
+  },
+});
 
 @injectIntl
 @withStyles(styles, { withTheme: true })
@@ -21,24 +40,21 @@ export default class GlobalDialog extends Component {
     const { globalDialog } = this.props.store.components;
     const { visible, title, heading, body } = globalDialog;
 
+    const formattedTitle = title && title.id ? intl.formatMessage(messages[title.id]) : title;
+    const formattedHeading = heading && heading.id ? intl.formatMessage(messages[heading.id]) : heading;
+    const formattedBody = body && body.id ? intl.formatMessage(messages[body.id]) : body;
+
     return visible && (
       <Dialog open={visible} onClose={globalDialog.reset}>
         <DialogTitle>
           <div className={classes.titleContainer}>
             <Error className={classes.titleIcon} />
-            {title.id ? intl.formatMessage({ id: title.id, defaultMessage: title.defaultMessage }) : title}
+            {formattedTitle}
           </div>
         </DialogTitle>
         <DialogContent>
-          <Typography className={classes.heading}>
-            {heading && heading.id
-              ? intl.formatMessage({ id: heading.id, defaultMessage: heading.defaultMessage })
-              : heading
-            }
-          </Typography>
-          <Typography className={classes.body}>
-            {body && body.id ? intl.formatMessage({ id: body.id, defaultMessage: body.defaultMessage }) : body}
-          </Typography>
+          <Typography className={classes.heading}>{formattedHeading}</Typography>
+          <Typography className={classes.body}>{formattedBody}</Typography>
         </DialogContent>
         <DialogActions>
           <Button color="primary" onClick={globalDialog.reset}>
