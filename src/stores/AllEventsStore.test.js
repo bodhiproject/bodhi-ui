52099f4e184fe7c72eb2ce655cae3e3fdf9673a5
Zhaoyang Han <bernardhan221@gmail.com>
2018-10-03 15:30:19 -0700

    checkout alleventstore test

--- src/stores/AllEventsStore.test.js
+++ src/stores/AllEventsStore.test.js
@@ -1,6 +1,9 @@
 import { observable } from 'mobx';
-import { Routes } from 'constants';
+import { Routes, SortBy } from 'constants';
+import cryptoRandomString from 'crypto-random-string';
 import AllEventsStore from './AllEventsStore';
+import { mockResetTopicList, mockResetOracleList, mockAddTopic, mockAddOracle } from '../network/graphql/queries/';
+
 jest.mock('../network/graphql/queries'); // block and manually mock our backend
 
 /** mock necessary http needed modules */
@@ -12,11 +15,12 @@ const localStorageMock = {
 const navigatorMock = {};
 global.navigator = navigatorMock;
 global.localStorage = localStorageMock;
+const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 describe('AllEventsStore', () => {
   let store;
   const addr = {
-    address: 'qSu4uU8MGp2Ya6j9kQZAtizUfC82aCvGT1',
+    address: cryptoRandomString(34),
     qtum: 2000,
     bot: 100,
   };
@@ -29,13 +33,28 @@ describe('AllEventsStore', () => {
   }); // mock the appstore
   beforeEach(() => {
     store = new AllEventsStore(app); // create a new instance before each test case
+    mockResetOracleList();
+    mockResetTopicList();
+    for (let i = 0; i < 18; i++) {
+      mockAddTopic({ txid: cryptoRandomString(64) });
+      mockAddOracle({
+        txid: cryptoRandomString(64),
+        amounts: [],
+        consensusThreshold: 100,
+        address: 'cryptoRandomString(40)',
+        topicAddress: 'cryptoRandomString(40)',
+        resultSetEndTime: 10,
+        endTime: 20,
+        options: [],
+      });
+    }
   });
 
   /** all following test cases target specifically to the mock backend, eg. network/graphql/__mocks__/queries.js */
   it('Init', async () => {
     const curListNum = store.list.length;
-    expect(curListNum).toBe(0);
     expect.assertions(4);
+    expect(curListNum).toBe(0);
     await store.init();
     expect(app.ui.location).toBe(Routes.ALL_EVENTS);
     expect(store.list.length).toBe(curListNum + store.limit);
@@ -45,64 +64,57 @@ describe('AllEventsStore', () => {
   it('Load More Events', async () => {
     await store.init();
     const curSkip = store.skip;
-    const curListNum = store.list.length;
     expect.assertions(6);
     expect(store.list.length).toBe(store.limit);
     expect(store.loadingMore).toBe(false);
     await store.loadMoreEvents();
-    expect(store.list.length).toBe((curListNum + store.limit) - 2); // mock backend only has 46 events in total
+    expect(store.list.length).toBe(36); // mock backend only has 36 events in total
     expect(store.loadingMore).toBe(false);
     expect(store.hasMore).toBe(false); // no events left in backend
     expect(store.skip).toBe(curSkip + store.limit);
   });
 
-  it('Reaction SortBy', (done) => {
-    expect.assertions(3);
+  it('Reaction SortBy', async () => {
+    expect.assertions(4);
     expect(store.list.length).toBe(0);
     app.ui.location = Routes.ALL_EVENTS;
-    app.sortBy = 'Yo';
-    setTimeout(() => {
-      expect(store.app.sortBy).toBe('Yo');
-      expect(store.list.length > 0).toBeTruthy();
-      done();
-    }, 500);
+    const newSortBy = app.sortBy === SortBy.ASCENDING ? SortBy.DESCENDING : SortBy.ASCENDING;
+    app.sortBy = newSortBy;
+    await sleep(100);
+    expect(store.app.ui.location).toBe(Routes.ALL_EVENTS);
+    expect(store.app.sortBy).toBe(newSortBy);
+    expect(store.list.length).toBe(store.limit);
   });
 
-  it('Reaction Wallet Addr', (done) => {
+  it('Reaction Wallet Addr', async () => {
     expect.assertions(2);
     expect(store.list.length).toBe(0);
     app.ui.location = Routes.ALL_EVENTS;
     const newAddr = {
-      address: 'qSu4uU8MGp2Ya6j9kQZAtizUfC82aCvGT2',
+      address: cryptoRandomString(34),
       qtum: 200,
       bot: 10,
     };
     app.wallet.addresses.push(newAddr);
-    setTimeout(() => {
-      expect(store.list.length > 0).toBeTruthy();
-      done();
-    }, 500);
+    await sleep(100);
+    expect(store.list.length).toBe(store.limit);
   });
 
-  it('Reaction BlockNum', (done) => {
+  it('Reaction BlockNum', async () => {
     expect.assertions(2);
     expect(store.list.length).toBe(0);
     app.ui.location = Routes.ALL_EVENTS;
     app.global.syncBlockNum = 1;
-    setTimeout(() => {
-      expect(store.list.length > 0).toBeTruthy();
-      done();
-    }, 500);
+    await sleep(100);
+    expect(store.list.length).toBe(store.limit);
   });
 
-  it('Reaction Refreshing', (done) => {
+  it('Reaction Refreshing', async () => {
     expect.assertions(2);
     expect(store.list.length).toBe(0);
     app.ui.location = Routes.ALL_EVENTS;
     app.refreshing = true;
-    setTimeout(() => {
-      expect(store.list.length > 0).toBeTruthy();
-      done();
-    }, 500);
+    await sleep(100);
+    expect(store.list.length).toBe(store.limit);
   });
 });
