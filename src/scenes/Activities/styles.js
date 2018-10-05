58367f432a1f461d758b24cc244603e57df2f0e2
Deric Walintukan <deric.walintukan@gmail.com>
2018-10-05 21:25:27 +0700

    Clear tx list in loggedIn reaction (#1034)
    * Clear tx list in loggedIn reaction
    
    * Remove border bottom from theme tab

--- src/scenes/Activities/styles.js
+++ src/scenes/Activities/styles.js
@@ -1,11 +1,9 @@
 const styles = (theme) => ({
   activitiesTabWrapper: {
-    background: theme.palette.background.paper,
     position: 'fixed',
     top: theme.sizes.navHeight,
     left: 0,
     right: 0,
-    height: '56px',
   },
   activitiesTabButton: {
     marginTop: '0px',
