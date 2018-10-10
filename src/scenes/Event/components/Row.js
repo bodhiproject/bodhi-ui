fa4bc74f4bef4d1b1d555fde899e45902520a6d0
Deric Walintukan <deric.walintukan@gmail.com>
2018-10-10 17:19:44 +0700

    Remove props passing to Row

--- src/scenes/Event/components/Row.js
+++ src/scenes/Event/components/Row.js
@@ -3,8 +3,8 @@ import { Grid, Paper, withStyles } from '@material-ui/core';
 
 import styles from './styles';
 
-export const Row = withStyles(styles)(({ children, ...props }) => (
-  <Paper elevation={0} {...props}>
+export const Row = withStyles(styles)(({ children }) => (
+  <Paper elevation={0}>
     <Grid container spacing={0}>{children}</Grid>
   </Paper>
 ));
