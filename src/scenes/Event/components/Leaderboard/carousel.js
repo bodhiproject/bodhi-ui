bb5089db720db81491f50fc33128224053556efa
Bowen Zhang <bowen@bohdi.network>
2018-10-22 11:01:03 -0700

    useless code remove

--- src/scenes/Event/components/Leaderboard/carousel.js
+++ src/scenes/Event/components/Leaderboard/carousel.js
@@ -1,14 +1,10 @@
-// @inheritedComponent Paper
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-// import { capitalize } from '../utils/helpers';
 
-export const styles = theme => ({
-  /* Styles applied to the root element. */
+export const styles = () => ({
   root: {
     display: 'flex',
     flexDirection: 'row',
@@ -16,24 +12,6 @@ export const styles = theme => ({
     alignItems: 'center',
     padding: 8,
   },
-  /* Styles applied to the root element if `position="bottom"`. */
-  positionBottom: {
-    position: 'fixed',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    zIndex: theme.zIndex.mobileStepper,
-  },
-  /* Styles applied to the root element if `position="top"`. */
-  positionTop: {
-    // position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    zIndex: theme.zIndex.mobileStepper,
-  },
-  /* Styles applied to the root element if `position="static"`. */
-  positionStatic: {},
   /* Styles applied to the dots container if `variant="dots"`. */
   dots: {
     display: 'flex',
@@ -41,22 +19,6 @@ export const styles = theme => ({
     backgroundColor: '#FFFFFF',
     color: '#585AFA',
     fontWeight: 'bold',
-  },
-  /* Styles applied to each dot if `variant="dots"`. */
-  dot: {
-    backgroundColor: theme.palette.action.disabled,
-    borderRadius: '50%',
-    width: 8,
-    height: 8,
-    margin: '0 2px',
-  },
-  /* Styles applied to a dot if `variant="dots"` and this is the active step. */
-  dotActive: {
-    backgroundColor: theme.palette.primary.main,
-  },
-  /* Styles applied to the Linear Progress component if `variant="progress"`. */
-  progress: {
-    width: '50%',
   },
 });
 
@@ -66,29 +28,25 @@ function CMobileStepper(props) {
     backButton,
     classes,
     className: classNameProp,
-    LinearProgressProps,
     nextButton,
-    position,
     steps,
-    variant,
     currentValue,
     ...other
   } = props;
 
   const className = classNames(
     classes.root,
-    classes.positionTop,
     classNameProp,
   );
 
   return (
     <Paper square elevation={0} className={className} {...other}>
       {backButton}
-      {variant === 'dots' && (
+      {
         <div className={classes.dots}>
           {currentValue}
         </div>
-      )}
+      }
       {nextButton}
     </Paper>
   );
@@ -114,23 +72,13 @@ CMobileStepper.propTypes = {
    */
   nextButton: PropTypes.node,
   /**
-   * Set the positioning type.
-   */
-  position: PropTypes.oneOf(['bottom', 'top', 'static']),
-  /**
    * The total steps.
    */
   steps: PropTypes.number.isRequired,
-  /**
-   * The variant to use.
-   */
-  variant: PropTypes.oneOf(['text', 'dots', 'progress']),
 };
 
 CMobileStepper.defaultProps = {
   activeStep: 0,
-  position: 'bottom',
-  variant: 'dots',
   backButton: undefined,
   nextButton: undefined,
 };
