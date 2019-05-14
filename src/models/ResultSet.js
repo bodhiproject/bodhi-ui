00c5c9b8719dabadb1ea6077266b2ad97dc472dd
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-14 16:24:27 +0700

    Finish result set model

--- src/models/ResultSet.js
+++ src/models/ResultSet.js
@@ -1,25 +1,21 @@
-import { TransactionType } from 'constants';
-/**
- * Model for ResultSet.
- */
+import { satoshiToDecimal } from '../../helpers/utility';
+
 export default class ResultSet {
-  type = ''
-  txid = '' // ResultSet ID assigned by the blockchain
-  blockNum = 0 // Block number when ResultSet was recorded on blockchain
-  blockTime = '' // Block timestamp for blockNum
-  senderAddress = '' // Sender's address
-  topicAddress = '' // Topic contract address associated with ResultSet
-  oracleAddress = ''
-  optionIdx = null // Result index used for ResultSet. eg. For a bet, this would be the result index the user bet on.
-  version = 0 // Current version of the contract. To manage deprecations later.
+  txid // Transaction ID returned when confirmed
+  txStatus // One of: [PENDING, SUCCESS, FAIL]
+  txReceipt // Transaction receipt returned when confirmed
+  blockNum // Block number when executed
+  block // Block info returned when confirmed
+  eventAddress // Event contract address
+  centralizedOracleAddress // Result setters address (only for eventRound 0)
+  resultIndex // Result index the bet was placed on
+  amount // Bet amount in decimals
+  amountSatoshi // Bet amount in satoshi
+  eventRound // Round of the event for the bet
 
   constructor(resultSet) {
     Object.assign(this, resultSet);
-    this.blockTime = resultSet.block.blockTime;
-    this.blockNum = resultSet.block.blockNum;
-    if (this.oracleAddress === null) this.type = TransactionType.FINALIZE_RESULT;
-    else this.type = TransactionType.SET_RESULT;
-    this.optionIdx = resultSet.resultIdx;
-    this.senderAddress = resultSet.fromAddress;
+    this.amount = satoshiToDecimal(resultSet.amount);
+    this.amountSatoshi = resultSet.amount;
   }
 }
