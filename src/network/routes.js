bd640aa5c1eee10f34a24506449b5215e229fb4e
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-14 18:47:15 +0700

    Update apis

--- src/network/routes.js
+++ src/network/routes.js
@@ -8,23 +8,28 @@ export const GRAPHQL = {
   SUBS: WS_ROUTE,
 };
 
+export const API = {
+  eventFactoryAddress: `${HTTP_ROUTE}/config-manager/event-factory-address`,
+  eventEscrowAmount: `${HTTP_ROUTE}/config-manager/event-escrow-amount`,
+  arbitrationLength: `${HTTP_ROUTE}/config-manager/arbitration-length`,
+  arbitrationRewardPercentage: `${HTTP_ROUTE}/config-manager/arbitration-reward-percentage`,
+  startingOracleThreshold: `${HTTP_ROUTE}/config-manager/starting-oracle-threshold`,
+  thresholdPercentIncrease: `${HTTP_ROUTE}/config-manager/threshold-percent-increase`,
+  calculateWinnings: `${HTTP_ROUTE}/multiple-results-event/calculate-winnings`,
+  version: `${HTTP_ROUTE}/multiple-results-event/version`,
+  round: `${HTTP_ROUTE}/multiple-results-event/round`,
+  resultIndex: `${HTTP_ROUTE}/multiple-results-event/result-index`,
+  consensusThreshold: `${HTTP_ROUTE}/multiple-results-event/consensus-threshold`,
+  arbitrationEndTime: `${HTTP_ROUTE}/multiple-results-event/arbitration-end-time`,
+  eventMetadata: `${HTTP_ROUTE}/multiple-results-event/event-metadata`,
+  centralizedMetadata: `${HTTP_ROUTE}/multiple-results-event/centralized-metadata`,
+  configMetadata: `${HTTP_ROUTE}/multiple-results-event/config-metadata`,
+  totalBets: `${HTTP_ROUTE}/multiple-results-event/total-bets`,
+  didWithdraw: `${HTTP_ROUTE}/multiple-results-event/did-withdraw`,
+  didWithdrawEscrow: `${HTTP_ROUTE}/multiple-results-event/did-withdraw-escrow`,
+};
+
 export default {
-  api: {
-    allowance: `${HTTP_ROUTE}/allowance`,
-    botBalance: `${HTTP_ROUTE}/bot-balance`,
-    eventEscrowAmount: `${HTTP_ROUTE}/event-escrow-amount`,
-    winnings: `${HTTP_ROUTE}/winnings`,
-    betBalances: `${HTTP_ROUTE}/bet-balances`,
-    voteBalances: `${HTTP_ROUTE}/vote-balances`,
-    transactionCost: `${HTTP_ROUTE}/transaction-cost`,
-    validateAddress: `${HTTP_ROUTE}/validate-address`,
-    getWalletInfo: `${HTTP_ROUTE}/get-wallet-info`,
-    unlockWallet: `${HTTP_ROUTE}/wallet-passphrase`,
-    encryptWallet: `${HTTP_ROUTE}/encrypt-wallet`,
-    backupWallet: `${HTTP_ROUTE}/backup-wallet`,
-    importWallet: `${HTTP_ROUTE}/import-wallet`,
-    walletPassphraseChange: `${HTTP_ROUTE}/wallet-passphrase-change`,
-  },
   insight: {
     totals: `${BASE_INSIGHT}/statistics/total`,
   },
