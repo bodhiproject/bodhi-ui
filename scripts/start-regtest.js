6b73e7a937fca324de6ae605f4cf841704fba553
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-14 13:20:05 +0700

    Fix start scripts

--- scripts/start-regtest.js
+++
@@ -1,7 +0,0 @@
-// Set regtest env vars
-process.env.CHAIN_NETWORK = 'regtest';
-process.env.API_HOSTNAME = 'test.puti.io';
-process.env.API_PORT = 5555;
-
-// Run start script
-require('./start');
--- scripts/start-testnet.js
+++ scripts/start-testnet.js
@@ -1,7 +1,7 @@
 // Set testnet env vars
 process.env.CHAIN_NETWORK = 'testnet';
-process.env.API_HOSTNAME = 'dev.puti.io';
-process.env.API_PORT = 6767;
+process.env.API_HOSTNAME = 'testnet.bodhipm.nakachain.org';
+process.env.API_PORT = 9999;
 
 // Run start script
 require('./start');
