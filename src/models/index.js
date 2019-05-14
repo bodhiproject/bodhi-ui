5bb3a3e57e570dca0967837d250db3e0af4be4fa
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-14 17:06:31 +0700

    Remove oracle from index

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,6 @@
 export { default as Bet } from './Bet';
 export { default as MultipleResultsEvent } from './MultipleResultsEvent';
 export { default as Option } from './Option';
-export { default as Oracle } from './Oracle';
 export { default as ResultSet } from './ResultSet';
 export { default as SyncInfo } from './SyncInfo';
 export { default as Transaction } from './Transaction';
