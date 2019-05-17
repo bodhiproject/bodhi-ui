1feb2c0351dd5346dcf31a30ccfcca4ea6494a12
Zhaoyang Han <bernardhan221@gmail.com>
2019-05-16 19:54:43 -0700

    Fix prediction page wrong status (#1158)

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -6,14 +6,14 @@ import { FormattedMessage, injectIntl, intlShape, defineMessages } from 'react-i
 import { Grid, Card, Divider, Typography, withStyles } from '@material-ui/core';
 import cx from 'classnames';
 import { sum, filter } from 'lodash';
-import { Phases, EventWarningType, TransactionStatus, TransactionType, EVENT_STATUS } from 'constants';
+import { EventWarningType, TransactionStatus, TransactionType, EVENT_STATUS } from 'constants';
 import { FavoriteButton } from 'components';
 import EventWarning from '../EventWarning';
 import styles from './styles';
 import { getEndTimeCountDownString } from '../../helpers';
 import carousel from '../../scenes/Event/components/Leaderboard/carousel';
 
-const { BETTING, RESULT_SETTING, VOTING, WITHDRAWING } = Phases;
+const { CREATED, BETTING, ORACLE_RESULT_SETTING, OPEN_RESULT_SETTING, ARBITRATION, WITHDRAWING } = EVENT_STATUS;
 const messages = defineMessages({
   pending: { id: 'str.pending', defaultMessage: 'Pending' },
   placeBet: { id: 'bottomButtonText.placeBet', defaultMessage: 'Place Bet' },
@@ -31,30 +31,28 @@ export default class EventCard extends Component {
   static propTypes = {
     classes: PropTypes.object.isRequired,
     index: PropTypes.number.isRequired,
-    endTime: PropTypes.string,
     intl: intlShape.isRequired, // eslint-disable-line react/no-typos
     onClick: PropTypes.func,
   };
 
   static defaultProps = {
-    endTime: undefined,
     onClick: null,
   };
 
   getAmountLabel = () => {
     const { status, token, amounts, nakaAmount, nbotAmount } = this.props.event;
     switch (status) {
-      case EVENT_STATUS.CREATED:
-      case EVENT_STATUS.BETTING:
-      case EVENT_STATUS.ORACLE_RESULT_SETTING:
-      case EVENT_STATUS.OPEN_RESULT_SETTING:
-      case EVENT_STATUS.ARBITRATION:
-      case EVENT_STATUS.WITHDRAWING: {
+      case CREATED:
+      case BETTING:
+      case ORACLE_RESULT_SETTING:
+      case OPEN_RESULT_SETTING:
+      case ARBITRATION:
+      case WITHDRAWING: {
         const amount = parseFloat(sum(amounts).toFixed(2));
         return `${amount} ${token}`;
       }
       default: {
-        console.error(`Unhandled phase: ${phase}`); // eslint-disable-line
+        console.error(`Unhandled status: ${status}`); // eslint-disable-line
         break;
       }
     }
@@ -63,31 +61,45 @@ export default class EventCard extends Component {
   getButtonText = () => {
     const { status } = this.props.event;
     switch (status) {
-      case EVENT_STATUS.CREATED:
-      case EVENT_STATUS.BETTING: return messages.placeBet;
-      case EVENT_STATUS.ORACLE_RESULT_SETTING:
-      case EVENT_STATUS.OPEN_RESULT_SETTING: return messages.setResult;
-      case EVENT_STATUS.ARBITRATION: return messages.arbitrate;
-      case EVENT_STATUS.WITHDRAWING: return messages.withdraw;
-      default: console.error(`Unhandled phase: ${phase}`); // eslint-disable-line
+      case CREATED:
+      case BETTING: return messages.placeBet;
+      case ORACLE_RESULT_SETTING:
+      case OPEN_RESULT_SETTING: return messages.setResult;
+      case ARBITRATION: return messages.arbitrate;
+      case WITHDRAWING: return messages.withdraw;
+      default: console.error(`Unhandled status: ${status}`); // eslint-disable-line
     }
   }
 
   get isWithdrawn() {
-    const { event: { phase, transactions } } = this.props;
-    if (phase !== Phases.WITHDRAWING) return false;
+    const { event: { status, transactions } } = this.props;
+    if (status !== WITHDRAWING) return false;
     const successTxs = filter(transactions, { status: TransactionStatus.SUCCESS, type: TransactionType.WITHDRAW });
 
     if (successTxs.length > 0) return true;
     return false;
   }
 
+  getEndTime = () => {
+    const { event, event: { status } } = this.props;
+    switch (status) {
+      case CREATED: return undefined;
+      case BETTING: return event.betEndTime;
+      case ORACLE_RESULT_SETTING: return event.resultSetEndTime;
+      case OPEN_RESULT_SETTING: return event.resultSetEndTime;
+      case ARBITRATION: return event.arbitrationEndTime;
+      case WITHDRAWING: return undefined;
+      default: console.error(`Unhandled status: ${status}`); // eslint-disable-line
+    }
+  }
+
   render() {
-    const { classes, index, onClick, store: { ui } } = this.props;
-    const { address, name, isPending, isUpcoming, txid, url, endTime, phase } = this.props.event;
+    const { classes, index, onClick, store: { ui, naka: { account } } } = this.props;
+    const { address, name, isPending, isUpcoming, txid, url, endTime, status } = this.props.event;
     const { locale, messages: localeMessages, formatMessage } = this.props.intl;
     const amountLabel = this.getAmountLabel();
     const { currentTimeUnix } = ui;
+    const endTime = this.getEndTime();
 
     return (
       <Grid item xs={12} sm={6} md={4} lg={3}>
@@ -95,9 +107,9 @@ export default class EventCard extends Component {
           <Card className={classes.eventCard} onClick={onClick}>
             <div className={cx(classes.eventCardBg, `bg${index % 8}`)}></div>
             <div className={cx(classes.eventCardSection, 'top')}>
-              {isPending && phase !== WITHDRAWING && <EventWarning id="str.pendingConfirmation" message="Pending Confirmation" />}
-              {isUpcoming && <EventWarning id="str.upcoming" message="Upcoming" type={EventWarningType.ORANGE} />}
-              {isPending && phase === WITHDRAWING && <EventWarning id="str.withdrawing" message="Withdrawning" type={EventWarningType.INFO} />}
+              {isPending() && status !== WITHDRAWING && <EventWarning id="str.pendingConfirmation" message="Pending Confirmation" />}
+              {isUpcoming(account) && <EventWarning id="str.upcoming" message="Upcoming" type={EventWarningType.ORANGE} />}
+              {isPending() && status === WITHDRAWING && <EventWarning id="str.withdrawing" message="Withdrawning" type={EventWarningType.INFO} />}
               {this.isWithdrawn && <EventWarning id="str.withdrawn" message="Withdrawn" type={EventWarningType.INFO} />}
               <div className={classes.eventCardNameBundle}>
                 <div className={classes.eventCardNameFlex}>
@@ -118,7 +130,7 @@ export default class EventCard extends Component {
                 <div className={classes.eventCardInfoItem}>
                   <i className={cx(classes.dashBoardCardIcon, 'icon iconfont icon-ic_timer')}></i>
                   {endTime !== undefined
-                    ? <Fragment>{getEndTimeCountDownString(this.props.event.endTime - currentTimeUnix, locale, localeMessages)}</Fragment>
+                    ? <Fragment>{getEndTimeCountDownString(endTime - currentTimeUnix, locale, localeMessages)}</Fragment>
                     : <FormattedMessage id="str.end" defaultMessage="Ended" />
                   }
                 </div>
@@ -126,7 +138,7 @@ export default class EventCard extends Component {
             </div>
             <Divider />
             <div className={cx(classes.eventCardSection, 'button')}>
-              {isUpcoming
+              {isUpcoming(account)
                 ? <FormattedMessage id="str.waitForResultSetting" defaultMessage="Waiting for result setting" />
                 : formatMessage(this.getButtonText())
               }
--- src/models/MultipleResultsEvent.js
+++ src/models/MultipleResultsEvent.js
@@ -53,13 +53,9 @@ export default class MultipleResultsEvent {
     this.url = `/event/${event.address}`;
   }
 
-  isUnconfirmed() {
-    return !this.address;
-  }
+  isUnconfirmed = () => !this.address;
 
-  isPending() {
-    return this.pendingTxs.total && this.pendingTxs.total > 0;
-  }
+  isPending = () => !!this.pendingTxs && this.pendingTxs.total && this.pendingTxs.total > 0;
 
   isUpcoming = (address) => this.status === EVENT_STATUS.ORACLE_RESULT_SETTING
     && address !== this.ownerAddress;
