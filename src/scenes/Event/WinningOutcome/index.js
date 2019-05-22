1fbb5d1e3de4cd7bed5e83aa606c5eec97a7a744
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-22 23:10:16 +0700

    Remove styled component in winning outcome

--- src/scenes/Event/WinningOutcome/index.js
+++ src/scenes/Event/WinningOutcome/index.js
@@ -1,44 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
-import { FormattedMessage, injectIntl, defineMessages } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import { Typography } from '@material-ui/core';
 import Icon from '../Icon';
 import Container from '../Container';
 import Label from '../Label';
 import { i18nToUpperCase } from '../../../helpers/i18nUtil';
 
-const messages = defineMessages({
-  withdrawDetailYouBetYouVoteMsg: {
-    id: 'withdrawDetail.youBetYouVote',
-    defaultMessage: 'You bet {naka} NAKA. You voted {nbot} NBOT.',
-  },
-});
-
 const WinningIcon = () => <Icon type='reward' />;
 
 const WinningOutcomeTitle = () => (
   <Label variant="body2">
-    <FormattedMessage id="str.winningOutcome" defaultMessage="Wining Outcome">
+    <FormattedMessage id="str.winningOutcome" defaultMessage="Winning Outcome">
       {(txt) => i18nToUpperCase(txt)}
     </FormattedMessage>
   </Label>
 );
 
-const OutcomeOption = styled(Typography)`
-  color: ${props => props.theme.palette.primary.main} !important;
-  font-size: ${props => props.theme.sizes.font.large.px} !important;
-  font-weight: ${props => props.theme.typography.fontWeightBold} !important;
-  margin-bottom: ${props => props.theme.padding.spaceX.px} !important;
-`;
+const WinningResult = ({ intl, eventPage }) => (
+  <Typography color="primary" variant="h6">
+    {eventPage.selectedOption.name === 'Invalid'
+      ? eventPage.event.localizedInvalid.parse(intl.locale)
+      : eventPage.selectedOption.name}
+  </Typography>
+);
 
-export default injectIntl(({ eventPage, intl }) => (
+const WinningOutcome = ({ intl, eventPage }) => (
   <Container>
     <WinningIcon />
     <WinningOutcomeTitle />
-    <OutcomeOption>{eventPage.selectedOption.name === 'Invalid' ? eventPage.event.localizedInvalid.parse(intl.locale) : eventPage.selectedOption.name}</OutcomeOption>
+    <WinningResult intl={intl} eventPage={eventPage} />
     {(eventPage.resultBetAmount || eventPage.resultVoteAmount) ? (
       <Typography variant="caption">
-        {intl.formatMessage(messages.withdrawDetailYouBetYouVoteMsg, { naka: eventPage.resultBetAmount, nbot: eventPage.resultVoteAmount })}
+        <FormattedMessage
+          id="withdrawDetail.youBetYouVote"
+          defaultMessage="You bet {naka} NAKA. You voted {nbot} NBOT."
+          values={{
+            naka: eventPage.resultBetAmount,
+            nbot: eventPage.resultVoteAmount,
+          }}
+        />
       </Typography>
     ) : (
       <Typography variant="caption">
@@ -49,4 +49,6 @@ export default injectIntl(({ eventPage, intl }) => (
       </Typography>
     )}
   </Container>
-));
+);
+
+export default injectIntl(WinningOutcome);
