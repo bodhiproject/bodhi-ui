16a630da7d9572e01518bba7314ce9fe5a7c1dbc
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-23 15:26:59 +0700

    Add mainnet build script

---
+++ scripts/build-mainnet.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Script should be run from root folder
+
+DEPLOY_PATH=/var/www/bodhi/mainnet/
+
+echo "Building Testnet UI..."
+node scripts/build-mainnet.js
+
+echo "Creating output path..."
+sudo mkdir -p $DEPLOY_PATH
+
+echo "Copying build folder to output path..."
+sudo cp -a build/. $DEPLOY_PATH
