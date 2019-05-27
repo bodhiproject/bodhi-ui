f45203812f7b08a90e56932bb547ce2848b524c0
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-27 17:03:12 +0700

    1221 change ascii to hex (#1222)
    * Change from ascii to toHex
    
    * Add contract metadata ver 2
    
    * Change default version to 2
    
    * Add padding for create event params
    
    * Remove log

--- src/config/token-exchange.js
+++
@@ -1,4 +0,0 @@
-export default Object.freeze({
-  address: '0x1111111111111111111111111111111111111111',
-  abi: [{ anonymous: false, inputs: [{ indexed: true, name: 'token', type: 'address' }, { indexed: true, name: 'exchanger', type: 'address' }, { indexed: false, name: 'newRate', type: 'uint256' }], name: 'RateChanged', type: 'event' }, { constant: true, inputs: [{ name: 'token', type: 'address' }, { name: 'exchanger', type: 'address' }], name: 'getRate', outputs: [{ name: 'rate', type: 'uint256' }], payable: false, stateMutability: 'view', type: 'function' }, { constant: false, inputs: [{ name: 'token', type: 'address' }, { name: 'newRate', type: 'uint256' }], name: 'setRate', outputs: [{ name: 'success', type: 'bool' }], payable: false, stateMutability: 'nonpayable', type: 'function' }],
-});
--- src/stores/TransactionStore.js
+++ src/stores/TransactionStore.js
@@ -1,7 +1,7 @@
 import { action, runInAction, toJS } from 'mobx';
 import { AbiCoder } from 'web3-eth-abi';
 import promisify from 'js-promisify';
-import { fromAscii } from 'web3-utils';
+import { toHex, padRight } from 'web3-utils';
 import {
   addPendingEvent,
   addPendingBet,
@@ -179,9 +179,9 @@ export default class TransactionStore {
       ];
       for (let i = 0; i < 10; i++) {
         if (createEventParams[1][i]) {
-          createEventParams[1][i] = fromAscii(createEventParams[1][i]);
+          createEventParams[1][i] = padRight(toHex(createEventParams[1][i]), 64);
         } else {
-          createEventParams[1][i] = fromAscii('');
+          createEventParams[1][i] = padRight(toHex(''), 64);
         }
       }
 
