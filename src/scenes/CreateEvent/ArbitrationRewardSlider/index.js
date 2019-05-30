f1dd85af95157cd8374d9a7e6fb0f576483d5655
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-30 15:57:32 +0700

    Add styling for slider

--- src/scenes/CreateEvent/ArbitrationRewardSlider/index.js
+++ src/scenes/CreateEvent/ArbitrationRewardSlider/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import { Typography, withStyles } from '@material-ui/core';
 import { Slider } from '@material-ui/lab';
+import { injectIntl, defineMessages } from 'react-intl';
+import styles from './styles';
+import { Section } from '../components';
 
+const messages = defineMessages({
+  arbitrationReward: {
+    id: 'create.arbitrationReward',
+    defaultMessage: 'Arbitration Reward',
+  },
+});
+
+@withStyles(styles, { withTheme: true })
+@injectIntl
 @inject('store')
 @observer
 export default class ArbitrationRewardSlider extends Component {
@@ -10,18 +23,32 @@ export default class ArbitrationRewardSlider extends Component {
     createEvent.arbitrationReward = value;
   }
 
+  renderBound = (value) => (
+    <Typography variant="body1" >
+      {value}
+    </Typography>
+  );
+
   render() {
-    const { store: { createEvent: { arbitrationReward } } } = this.props;
+    const {
+      classes,
+      store: { createEvent: { arbitrationReward } },
+    } = this.props;
     return (
-      <div>
-        <Slider
-          min={1}
-          max={50}
-          value={arbitrationReward}
-          step={1}
-          onChange={this.onChange}
-        />
-      </div>
+      <Section column title={messages.arbitrationReward}>
+        <div className={classes.contentContainer}>
+          {this.renderBound(1)}
+          <Slider
+            className={classes.slider}
+            min={1}
+            max={50}
+            value={arbitrationReward}
+            step={1}
+            onChange={this.onChange}
+          />
+          {this.renderBound(50)}
+        </div>
+      </Section>
     );
   }
 }
---
+++ src/scenes/CreateEvent/ArbitrationRewardSlider/styles.js
@@ -0,0 +1,12 @@
+export default (theme) => ({
+  contentContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  slider: {
+    flex: 1,
+    // alignSelf: 'center',
+    margin: `0 ${theme.padding.space3X.px}`,
+  },
+});
