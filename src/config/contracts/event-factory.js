b6eb045154bf7b6e119ca421311b3a9fc559116d
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-03 17:19:34 +0700

    Update v4 metadata

--- src/config/contracts/event-factory.js
+++ src/config/contracts/event-factory.js
@@ -5,6 +5,13 @@
  * been synced.
  */
 module.exports = {
+  4: {
+    mainnet: '',
+    mainnetDeployBlock: 0,
+    testnet: '0x45c537f157986dfaa7026af3f055bba77b970fc8',
+    testnetDeployBlock: 3735500,
+    abi: [{"inputs": [{"name": "configManager","type": "address"}],"payable": false,"stateMutability": "nonpayable","type": "constructor"},{"anonymous": false,"inputs": [{"indexed": true,"name": "eventAddress","type": "address"},{"indexed": true,"name": "ownerAddress","type": "address"}],"name": "MultipleResultsEventCreated","type": "event"},{"constant": false,"inputs": [{"name": "from","type": "address"},{"name": "value","type": "uint256"},{"name": "data","type": "bytes"}],"name": "tokenFallback","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": false,"inputs": [],"name": "withdrawEscrow","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [],"name": "didWithdraw","outputs": [{"name": "","type": "bool"}],"payable": false,"stateMutability": "view","type": "function"}],
+  },
   3: {
     mainnet: '',
     mainnetDeployBlock: 0,
