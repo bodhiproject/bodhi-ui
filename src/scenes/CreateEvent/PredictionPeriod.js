6360abe067184e6af274af218bae0652622a8bb7
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-04 17:11:29 +0700

    Add state for prediction times

--- src/scenes/CreateEvent/PredictionPeriod.js
+++ src/scenes/CreateEvent/PredictionPeriod.js
@@ -15,17 +15,36 @@ const messages = defineMessages({
 @inject('store')
 @observer
 export default class PredictionPeriod extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startTime: props.store.createEvent.prediction.startTime,
+      endTime: props.store.createEvent.prediction.endTime,
+    };
+  }
+
+  onStartTimeChange = (date) => {
+    this.setState({ startTime: date.unix() });
+    this.props.store.createEvent.prediction.startTime = date.unix();
+  }
+
+  onEndTimeChange = (date) => {
+    this.setState({ endTime: date.unix() });
+    this.props.store.createEvent.prediction.endTime = date.unix();
+  }
+
   render() {
     const {
       store: {
         createEvent,
         createEvent: {
           predictionPeriod,
-          prediction: { startTime, endTime },
+          // prediction: { startTime, endTime },
           error: { prediction: { startTime: startTimeErr, endTime: endTimeErr } },
         },
       },
     } = this.props;
+    const { startTime, endTime } = this.state;
     console.log('NAKA: PredictionPeriod -> render -> startTime', startTime);
     console.log('NAKA: PredictionPeriod -> render -> endTime', endTime);
 
@@ -35,13 +54,13 @@ export default class PredictionPeriod extends Component {
           title={TimeCardTitle.START_TIME}
           dateUnix={startTime}
           error={startTimeErr}
-          onChange={(date) => date.isValid && (createEvent.prediction.startTime = date.unix())}
+          onChange={this.onStartTimeChange}
         />
         <DateTimeCard
           title={TimeCardTitle.END_TIME}
           dateUnix={endTime}
           error={endTimeErr}
-          onChange={(date) => date.isValid && (createEvent.prediction.startTime = date.unix())}
+          onChange={this.onEndTimeChange}
         />
       </Section>
     );
