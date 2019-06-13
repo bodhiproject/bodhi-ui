ed011e342763a88410080f21dce31d5df29411da
Bernard <bernardhan221@gmail.com>
2019-06-13 16:05:40 -0700

    Remove see all shadow

--- src/components/SeeAllButton/index.js
+++ src/components/SeeAllButton/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { inject, observer } from 'mobx-react';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { withStyles, Typography } from '@material-ui/core';
 import { KeyboardArrowRight } from '@material-ui/icons';
 import { FormattedMessage, injectIntl } from 'react-intl';
@@ -17,18 +17,20 @@ export default class BackButton extends Component {
     url: PropTypes.string.isRequired,
   };
 
+  handleClick = (url) => {
+    this.props.history.push(url);
+  }
+
   render() {
     const { classes, url } = this.props;
 
     return (
-      <Link to={url}>
-        <div className={classes.bottomButton}>
-          <Typography color='textPrimary' className={classes.bottomButtonText}>
-            <FormattedMessage id="str.seeAll" defaultMessage="See All " />
-            <KeyboardArrowRight className={classes.bottomButtonIcon} />
-          </Typography>
-        </div>
-      </Link>
+      <div className={classes.bottomButton} onClick={() => this.handleClick(url)}>
+        <Typography color='textPrimary' className={classes.bottomButtonText}>
+          <FormattedMessage id="str.seeAll" defaultMessage="See All " />
+          <KeyboardArrowRight className={classes.bottomButtonIcon} />
+        </Typography>
+      </div>
     );
   }
 }
